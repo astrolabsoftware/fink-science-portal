13d4a6cd32292874b6c6631afce178fb40d60220
Julien <peloton@lal.in2p3.fr>
2020-10-01 09:28:18 +0200

    Add header, and clean code

--- index.py
+++ index.py
@@ -1,27 +1,45 @@
+# Copyright 2020 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
-# must add this line in order for the app to be deployed successfully on Heroku
 from app import server
 from app import app
+
 # import all pages in the app
-#from apps import global_situation, singapore, home
 from apps import home, explorer, grafink, alert, about
 
 # building the navigation bar
-# https://github.com/facultyai/dash-bootstrap-components/blob/master/examples/advanced-component-usage/Navbars.py
 dropdown = dbc.DropdownMenu(
     children=[
         dbc.DropdownMenuItem("About", href="/about"),
         dbc.DropdownMenuItem("Fink Website", href="https://fink-broker.org/"),
-        dbc.DropdownMenuItem("GitHub", href="https://github.com/astrolabsoftware/fink-broker"),
-        dbc.DropdownMenuItem("Fink documentation", href="https://fink-broker.readthedocs.io/en/latest/"),
+        dbc.DropdownMenuItem(
+            "GitHub",
+            href="https://github.com/astrolabsoftware/fink-broker"
+        ),
+        dbc.DropdownMenuItem(
+            "Fink documentation",
+            href="https://fink-broker.readthedocs.io/en/latest/"
+        ),
     ],
-    nav = True,
-    in_navbar = True,
-    label = "Explore",
+    nav=True,
+    in_navbar=True,
+    label="Explore",
 )
 
 navbar = dbc.Navbar(
@@ -31,8 +49,12 @@ navbar = dbc.Navbar(
                 # Use row and col to control vertical alignment of logo / brand
                 dbc.Row(
                     [
-                        #dbc.Col(html.Img(src="/assets/Fink_PrimaryLogo_WEB.png", height="50px")),
-                        dbc.Col(dbc.NavbarBrand("Fink Sience portal", className="ml-2")),
+                        dbc.Col(
+                            dbc.NavbarBrand(
+                                "Fink Sience portal",
+                                className="ml-2"
+                            )
+                        ),
                     ],
                     align="center",
                     no_gutters=True,
@@ -43,7 +65,9 @@ navbar = dbc.Navbar(
             dbc.Collapse(
                 dbc.Nav(
                     # right align dropdown menu with ml-auto className
-                    [dropdown], className="ml-auto", navbar=True
+                    [dropdown],
+                    className="ml-auto",
+                    navbar=True
                 ),
                 id="navbar-collapse2",
                 navbar=True,
@@ -55,17 +79,17 @@ navbar = dbc.Navbar(
     className="mb-4",
 )
 
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
-
-for i in [2]:
-    app.callback(
-        Output(f"navbar-collapse{i}", "is_open"),
-        [Input(f"navbar-toggler{i}", "n_clicks")],
-        [State(f"navbar-collapse{i}", "is_open")],
-    )(toggle_navbar_collapse)
+# def toggle_navbar_collapse(n, is_open):
+#     if n:
+#         return not is_open
+#     return is_open
+#
+# for i in [2]:
+#     app.callback(
+#         Output(f"navbar-collapse{i}", "is_open"),
+#         [Input(f"navbar-toggler{i}", "n_clicks")],
+#         [State(f"navbar-collapse{i}", "is_open")],
+#     )(toggle_navbar_collapse)
 
 # embedding the navigation bar
 app.layout = html.Div([
@@ -88,6 +112,7 @@ def display_page(pathname):
         return alert.layout(pathname)
     else:
         return home.layout
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
