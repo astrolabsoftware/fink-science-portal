9d2bf5a63aaeffe9662eb336b13b5924e74e99bf
Julien <peloton@lal.in2p3.fr>
2020-10-05 11:23:00 +0200

    Fix missing import

--- apps/utils.py
+++ apps/utils.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
+import pandas as pd
 import gzip
 import io
 from astropy.io import fits
