3dddecfebf83752cd5bad45e849aa9dd49da42ec
Julien <peloton@lal.in2p3.fr>
2020-10-05 12:40:11 +0200

    Add threshold for score plot

--- apps/plotting.py
+++ apps/plotting.py
@@ -24,6 +24,19 @@ from apps.utils import extract_row, extract_properties
 
 from app import client, app
 
+colors_ = [
+    '#1f77b4',  # muted blue
+    '#ff7f0e',  # safety orange
+    '#2ca02c',  # cooked asparagus green
+    '#d62728',  # brick red
+    '#9467bd',  # muted purple
+    '#8c564b',  # chestnut brown
+    '#e377c2',  # raspberry yogurt pink
+    '#7f7f7f',  # middle gray
+    '#bcbd22',  # curry yellow-green
+    '#17becf'   # blue-teal
+]
+
 layout_lightcurve = dict(
     autosize=True,
     automargin=True,
@@ -178,6 +191,16 @@ def draw_scores(data: java.util.TreeMap) -> dict:
                     'size': 12,
                     'color': '#d62728',
                     'symbol': 'circle-open-dot'}
+            },
+            {
+                'y': 0.5,
+                'mode': 'lines',
+                'name': '',
+                'line': {
+                    'color': 'black',
+                    'width': 2.5,
+                    'dash': 'dash'
+                }
             }
         ],
         "layout": layout_scores
