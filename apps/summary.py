e8d7c8b96eea7c5a5a4d32471b7fa3026b34227b
Julien <peloton@lal.in2p3.fr>
2020-11-03 11:18:46 +0100

    store object data inside dummy div

--- apps/summary.py
+++ apps/summary.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import visdcc
 
