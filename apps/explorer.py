fb53ec84ef6764a86ae90d326f1fdbdd2534a456
Julien <peloton@lal.in2p3.fr>
2020-11-24 13:14:32 +0100

    Improve documentation for explorer (#38)
    * tooltip for conesearch
    
    * Set tooltip to the right
    
    * Set tooltip layout
    
    * Set tooltip layout
    
    * Set tooltip layout
    
    * Set tooltip layout
    
    * Update the doc of explorer
    
    * Update the doc of explorer
    
    * Update the layout
    
    * Try another way to open help
    
    * Allow help to be dismissable
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast
    
    * no results toast

--- apps/explorer.py
+++ apps/explorer.py
@@ -37,32 +37,165 @@ from apps.utils import extract_fink_classification
 from apps.utils import markdownify_objectid
 
 msg = """
-Fill one of the fields on the left, and click on the _Submit Query_ button.
+![logoexp](/assets/Fink_PrimaryLogo_WEB.png)
 
-* _**Search by Object ID:** Enter a valid object ID to access its data, e.g. try_:
-  * ZTF19acmdpyr, ZTF19acnjwgm, ZTF17aaaabte, ZTF20abqehqf, ZTF18acuajcr
-* _**Conesearch:** Perform a conesearch around a position on the sky given by (RA, Dec, radius). The initializer for RA/Dec is very flexible and supports inputs provided in a number of convenient formats. The following ways of initializing a conesearch are all equivalent (radius in arcsecond):_
-  * (271.3914265, 45.2545134, 5), (271d23m29.1354s, 45d15m16.2482s, 5), (18h05m33.9424s, +45d15m16.2482s, 5), (18 05 33.9424, +45 15 16.2482, 5), (18:05:33.9424, 45:15:16.2482, 5)
-* _**Search by Date:** Choose a starting date and a time window to see all alerts in this period. Dates are in UTC, and the time window in minutes. Example of valid search:_
-  * 2019-11-03 02:40:00
-* _**Get latest 100 alerts by class:** Choose a class of interest using the drop-down menu to see the 100 latest alerts processed by Fink._
+Fill one of the fields on the left, and click on the _Submit Query_ button. You can access help by clicking on the buttons at the left of each field.
 
-_The table shows:_
+The table shows:
 
-- _objectId: Unique identifier for this object_
-- _RA: Right Ascension of candidate; J2000 (deg)_
-- _Dec: Declination of candidate; J2000 (deg)_
-- _last seen: last date the object has been seen by Fink_
-- _classification: Classification inferred by Fink (Supernova candidate, Microlensing candidate, Solar System Object, SIMBAD class, ...)_
-- _ndethist: Number of spatially coincident detections falling within 1.5 arcsec going back to the beginning of the survey; only detections that fell on the same field and readout-channel ID where the input candidate was observed are counted. All raw detections down to a photometric S/N of ~ 3 are included._
+- objectId: Unique identifier for this object
+- RA: Right Ascension of candidate; J2000 (deg)
+- Dec: Declination of candidate; J2000 (deg)
+- last seen: last date the object has been seen by Fink
+- classification: Classification inferred by Fink (Supernova candidate, Microlensing candidate, Solar System Object, SIMBAD class, ...)
+- ndethist: Number of spatially coincident detections falling within 1.5 arcsec going back to the beginning of the survey; only detections that fell on the same field and readout-channel ID where the input candidate was observed are counted. All raw detections down to a photometric S/N of ~ 3 are included.
+"""
+
+msg_conesearch = """
+Perform a conesearch around a position on the sky given by (RA, Dec, radius).
+The initializer for RA/Dec is very flexible and supports inputs provided in a number of convenient formats.
+The following ways of initializing a conesearch are all equivalent (radius in arcsecond):
+
+* 271.3914265, 45.2545134, 5
+* 271d23m29.1354s, 45d15m16.2482s, 5
+* 18h05m33.9424s, +45d15m16.2482s, 5
+* 18 05 33.9424, +45 15 16.2482, 5
+* 18:05:33.9424, 45:15:16.2482, 5
+"""
+
+msg_objectid = """
+Enter a valid object ID to access its data, e.g. try:
+
+* ZTF19acmdpyr, ZTF19acnjwgm, ZTF17aaaabte, ZTF20abqehqf, ZTF18acuajcr
+"""
+
+msg_date = """
+Choose a starting date and a time window to see all alerts in this period.
+Dates are in UTC, and the time window in minutes.
+Example of valid search:
+
+* 2019-11-03 02:40:00
+"""
+
+msg_latest_alerts = """
+Choose a class of interest using the drop-down menu to see the 100 latest alerts processed by Fink.
 """
 
 simbad_types = pd.read_csv('assets/simbad_types.csv', header=None)[0].values
 simbad_types = np.sort(simbad_types)
 
+
+noresults_toast = dbc.Toast(
+    "",
+    header="",
+    id="noresults-toast",
+    icon="danger",
+    dismissable=True,
+    is_open=False,
+    style={"position": "fixed", "top": 66, "right": 10, "width": 350},
+)
+
+@app.callback(
+    [
+        Output("noresults-toast", "is_open"),
+        Output("noresults-toast", "children"),
+        Output("noresults-toast", "header")
+    ],
+    [
+        Input("submit_query", "n_clicks"),
+        Input("table", "children"),
+        Input("objectid", "value"),
+        Input("conesearch", "value"),
+        Input('startdate', 'value'),
+        Input('window', 'value'),
+        Input('class-dropdown', 'value')
+    ]
+)
+def open_noresults(n, table, objectid, radecradius, startdate, window, alert_class):
+    """ Toast to warn the user about the fact that we found no results
+    """
+    # Trigger the query only if the submit button is pressed.
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    if 'submit_query' not in changed_id:
+        raise PreventUpdate
+
+    id_click = (objectid is not None) and (objectid != '')
+    conesearch_click = (radecradius is not None) and (radecradius != '')
+    date_click = (startdate is not None) and (startdate != '')
+    class_click = (alert_class is not None) and (alert_class != '')
+
+    # multiple queries
+    if np.sum([id_click, conesearch_click, date_click, class_click]) > 1:
+        m = []
+        for name, condition in zip(["Search by Object ID", "Conesearch", "Search by Date", "Get latest 100 alerts by class"], [id_click, conesearch_click, date_click, class_click]):
+            if condition:
+                m.append(name)
+        header = "Multi index search"
+        text = "Searching along multiple fields is not yet allowed. Keep only one among {}".format(m)
+        return True, text, header
+
+    # ugly hack on the type
+    if n and (table['namespace'] == 'dash_html_components'):
+        if id_click:
+            header = "Search by Object ID"
+            text = "{} not found".format(objectid)
+        elif conesearch_click:
+            header = "Conesearch"
+            text = "No alerts found for (RA, Dec, radius) = {}".format(
+                radecradius
+            )
+        elif date_click:
+            header = "Search by Date"
+            if window:
+                jd_start = Time(startdate).jd
+                jd_end = jd_start + TimeDelta(window * 60, format='sec').jd
+
+                text = "No alerts found between {} and {}".format(
+                    Time(jd_start, format='jd').iso,
+                    Time(jd_end, format='jd').iso
+                )
+            else:
+                text = "You need to set a window (in minutes)"
+        elif class_click:
+            header = "Get latest 100 alerts by class"
+            # start of the Fink operations
+            jd_start = Time('2019-11-01 00:00:00').jd
+            jd_stop = Time.now().jd
+
+            text = "No alerts for class {} in between {} and {}".format(
+                alert_class,
+                Time(jd_start, format='jd').iso,
+                Time(jd_stop, format='jd').iso
+            )
+        return True, text, header
+    return False, "", ""
+
 object_id = dbc.FormGroup(
     [
-        dbc.Label("Search by Object ID"),
+        dbc.Label(
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dbc.Button(
+                            "",
+                            color="secondary",
+                            id='open-objectid',
+                            outline=True,
+                            size="sm",
+                            style=dict(height='10%')
+                        ), width=1),
+                    dbc.Col("Search by Object ID"),
+                ],
+            )
+        ),
+        dbc.Toast(
+            [dcc.Markdown(msg_objectid, className="mb-0")],
+            id="objectid-toast",
+            header="Help",
+            icon="light",
+            dismissable=True,
+            is_open=False
+        ),
         dbc.Input(
             placeholder="e.g. ZTF19acmdpyr",
             type="text",
@@ -73,9 +206,43 @@ object_id = dbc.FormGroup(
     ], style={'width': '100%', 'display': 'inline-block'}
 )
 
+@app.callback(
+    Output("objectid-toast", "is_open"),
+    [Input("open-objectid", "n_clicks")],
+    [State("objectid-toast", "is_open")],
+)
+def open_objectid(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
 conesearch = dbc.FormGroup(
     [
-        dbc.Label("Conesearch"),
+        dbc.Label(
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dbc.Button(
+                            "",
+                            color="secondary",
+                            id='open-conesearch',
+                            outline=True,
+                            size="sm",
+                            style=dict(height='10%')
+                        ), width=1
+                    ),
+                    dbc.Col("Conesearch", width=1),
+                ],
+            )
+        ),
+        dbc.Toast(
+            [dcc.Markdown(msg_conesearch, className="mb-0")],
+            id="conesearch-toast",
+            header="Help",
+            icon="light",
+            dismissable=True,
+            is_open=False
+        ),
         dbc.Input(
             placeholder="ra, dec, radius",
             type="text",
@@ -86,9 +253,43 @@ conesearch = dbc.FormGroup(
     ], style={'width': '100%', 'display': 'inline-block'}
 )
 
+@app.callback(
+    Output("conesearch-toast", "is_open"),
+    [Input("open-conesearch", "n_clicks")],
+    [State("conesearch-toast", "is_open")],
+)
+def open_conesearch(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
 date_range = dbc.FormGroup(
     [
-        dbc.Label("Search by Date"),
+        dbc.Label(
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dbc.Button(
+                            "",
+                            color="secondary",
+                            id='open-date',
+                            outline=True,
+                            size="sm",
+                            style=dict(height='10%')
+                        ), width=1
+                    ),
+                    dbc.Col("Search by Date", width=10),
+                ],
+            )
+        ),
+        dbc.Toast(
+            [dcc.Markdown(msg_date, className="mb-0")],
+            id="date-toast",
+            header="Help",
+            icon="light",
+            dismissable=True,
+            is_open=False
+        ),
         html.Br(),
         dbc.Input(
             placeholder="YYYY-MM-DD HH:mm:ss",
@@ -108,9 +309,43 @@ date_range = dbc.FormGroup(
     ], style={'width': '100%', 'display': 'inline-block'}
 )
 
+@app.callback(
+    Output("date-toast", "is_open"),
+    [Input("open-date", "n_clicks")],
+    [State("date-toast", "is_open")],
+)
+def open_date(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
 dropdown = dbc.FormGroup(
     [
-        dbc.Label("Get latest 100 alerts by class"),
+        dbc.Label(
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dbc.Button(
+                            "",
+                            color="secondary",
+                            id='open-latest-alerts',
+                            outline=True,
+                            size="sm",
+                            style=dict(height='10%')
+                        ), width=1
+                    ),
+                    dbc.Col(dbc.Label("Get latest 100 alerts by class"), width=11),
+                ], justify='start',
+            ), style={'width': '100%', 'display': 'inline-block'}
+        ),
+        dbc.Toast(
+            [dcc.Markdown(msg_latest_alerts, className="mb-0")],
+            id="latest-alerts-toast",
+            header="Help",
+            icon="light",
+            dismissable=True,
+            is_open=False
+        ),
         dcc.Dropdown(
             id='class-dropdown',
             options=[
@@ -130,6 +365,16 @@ dropdown = dbc.FormGroup(
         )
     ], style={'width': '100%', 'display': 'inline-block'}
 )
+
+@app.callback(
+    Output("latest-alerts-toast", "is_open"),
+    [Input("open-latest-alerts", "n_clicks")],
+    [State("latest-alerts-toast", "is_open")],
+)
+def open_latest_alerts(n, is_open):
+    if n:
+        return not is_open
+    return is_open
 
 submit_button = dbc.Button(
     'Submit Query',
@@ -201,7 +446,7 @@ layout = html.Div(
                     ], width=9),
                 ]),
             ], className="mb-8", fluid=True, style={'width': '95%'}
-        )
+        ), noresults_toast
     ], className='home', style={
         'background-image': 'linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url(/assets/background.png)',
         'background-size': 'contain'
@@ -245,6 +490,14 @@ def construct_table(n_clicks, objectid, radecradius, startdate, window, alert_cl
     # Trigger the query only if the submit button is pressed.
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
     if 'submit_query' not in changed_id:
+        raise PreventUpdate
+
+    id_click = (objectid is not None) and (objectid != '')
+    conesearch_click = (radecradius is not None) and (radecradius != '')
+    date_click = (startdate is not None) and (startdate != '')
+    class_click = (alert_class is not None) and (alert_class != '')
+
+    if np.sum([id_click, conesearch_click, date_click, class_click]) > 1:
         raise PreventUpdate
 
     # wrong query
