4c328cddda4529e91d04a1c27260dda12d9bafef
Julien <peloton@lal.in2p3.fr>
2020-11-25 07:59:31 +0100

    separate labels and values for simbad types (#46)

--- apps/explorer.py
+++ apps/explorer.py
@@ -81,8 +81,8 @@ msg_latest_alerts = """
 Choose a class of interest using the drop-down menu to see the 100 latest alerts processed by Fink.
 """
 
-simbad_types = pd.read_csv('assets/simbad_types.csv', header=None)[0].values
-simbad_types = np.sort([i.capitalize() for i in simbad_types])
+simbad_types = np.sort(pd.read_csv('assets/simbad_types.csv', header=None)[0].values)
+simbad_types_display = [i.capitalize() for i in simbad_types]
 
 
 @app.callback(
@@ -377,7 +377,7 @@ dropdown = dbc.FormGroup(
                 {'label': 'Solar System Object candidates', 'value': 'Solar System'},
                 {'label': 'Ambiguous', 'value': 'Ambiguous'},
                 {'label': 'Simbad crossmatch', 'disabled': True, 'value': 'None'},
-                *[{'label': simtype, 'value': simtype} for simtype in simbad_types]
+                *[{'label': label, 'value': value} for label, values in zip(simbad_types_display, simbad_types)]
             ],
             searchable=True,
             clearable=True,
