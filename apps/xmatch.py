085b8ded4ea24e91e18bb54edc7e72e4e9279e90
Julien <peloton@lal.in2p3.fr>
2020-11-27 11:34:53 +0100

    missing import

--- apps/xmatch.py
+++ apps/xmatch.py
@@ -19,6 +19,7 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
+import dash_bootstrap_components as dbc
 
 import pandas as pd
 import healpy as hp
