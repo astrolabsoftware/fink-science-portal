74b7ca0f45d23cd86de8335db8ffaa1dbe8adb6c
Julien <peloton@lal.in2p3.fr>
2020-12-01 14:00:02 +0100

    Fix index if r is the only band (#68)

--- apps/plotting.py
+++ apps/plotting.py
@@ -828,18 +828,23 @@ def plot_mulens(n_clicks, object_data):
             plot_filt1 = {}
 
         if '2' in np.unique(pdf['i:fid'].values):
+            # only filter r
+            if len(np.unique(pdf['i:fid'].values)) == 1:
+                index = 0
+            else:
+                index = 1
             plot_filt2 = {
-                'x': [convert_jd(t, to='iso') for t in normalised_lightcurves[1][:, 0]],
-                'y': normalised_lightcurves[1][:, 1],
+                'x': [convert_jd(t, to='iso') for t in normalised_lightcurves[index][:, 0]],
+                'y': normalised_lightcurves[index][:, 1],
                 'error_y': {
                     'type': 'data',
-                    'array': normalised_lightcurves[1][:, 2],
+                    'array': normalised_lightcurves[index][:, 2],
                     'visible': True,
                     'color': '#ff7f0e'
                 },
                 'mode': 'markers',
                 'name': 'r band',
-                'text': [convert_jd(t, to='iso') for t in normalised_lightcurves[1][:, 0]],
+                'text': [convert_jd(t, to='iso') for t in normalised_lightcurves[index][:, 0]],
                 'marker': {
                     'size': 12,
                     'color': '#ff7f0e',
