c4db29720e2fc214adbd210bba6f81e5a5a77cc3
Julien <peloton@lal.in2p3.fr>
2020-12-10 11:49:59 +0100

    Fix typo

--- apps/explorer.py
+++ apps/explorer.py
@@ -736,7 +736,7 @@ def construct_table(n_clicks, reset_button, objectid, radecradius, startdate, wi
     # )
     #
     # # Display only the last alert
-    pdfs = pdfs.loc[pdfs.groupby('objectId')['i:jd'].idxmax()]
+    pdfs = pdfs.loc[pdfs.groupby('i:objectId')['i:jd'].idxmax()]
     # pdfs['last seen'] = pdfs['last seen'].apply(convert_jd)
 
     # round numeric values for better display
