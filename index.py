3ea344f7d7d9ca5c863ffcb6d3e508cd5d07944b
Julien <peloton@lal.in2p3.fr>
2020-12-18 13:02:18 +0100

    Fix bug...

--- index.py
+++ index.py
@@ -60,8 +60,7 @@ navbar = dbc.Navbar(
         dbc.Collapse(
             dbc.Nav(
                 # right align dropdown menu with ml-auto className
-                dbc.Button('API', href="http://134.158.75.151:24000/api"),
-                [dropdown],
+                [dbc.Button('API', href="http://134.158.75.151:24000/api"), dropdown],
                 navbar=True,
                 vertical=True
             ),
