6f94f88dbef31b7bb1d7a14d74ed0ac047d5263c
Julien <peloton@lal.in2p3.fr>
2020-12-18 11:37:27 +0100

    Missing imports

--- apps/cards.py
+++ apps/cards.py
@@ -15,6 +15,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
 from app import client, app
 
