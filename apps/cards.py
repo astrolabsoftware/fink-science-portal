2688dcefdf9f2745aa6b132d795ab2c07e867e1c
Julien <peloton@lal.in2p3.fr>
2021-03-29 17:59:01 +0200

    fix missing import

--- apps/cards.py
+++ apps/cards.py
@@ -16,6 +16,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
+import dash_table
 import visdcc
 
 from app import app
