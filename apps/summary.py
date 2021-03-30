dde0ef7ebd849437fcaeb19a0869064f7a66478f
Julien <peloton@lal.in2p3.fr>
2021-03-30 16:13:57 +0200

    New download button

--- apps/summary.py
+++ apps/summary.py
@@ -194,7 +194,7 @@ def layout(name):
                                     'height': '25pc'
                                 }
                             ),
-                            download_object_modal(pdf['i:objectId'].values[0])
+                            *download_object_modal(pdf['i:objectId'].values[0])
                         ], width={"size": 3},
                     ),
                     dbc.Col(tabs(pdf), width=8)
