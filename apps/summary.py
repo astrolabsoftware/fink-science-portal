ab390ba5ea5e26214472b1f097c53056f30a4eec
Julien <peloton@lal.in2p3.fr>
2021-05-26 16:36:48 +0200

    Try class bar

--- apps/summary.py
+++ apps/summary.py
@@ -62,13 +62,13 @@ def tab1_content(pdf):
     fig = go.Figure()
 
     for i in range(0, len(x_data[0])):
-        for xd, yd in zip(x_data, y_data):
+        for xd, yd, label in zip(x_data, y_data, to_labels):
             fig.add_trace(
                 go.Bar(
                     x=[xd[i]], y=[yd],
                     orientation='h',
                     marker=dict(
-                        color=colors[xd[i]],
+                        color=colors[label[i]],
                         line=dict(color='rgb(248, 248, 249)', width=1)
                     )
                 )
@@ -95,6 +95,83 @@ def tab1_content(pdf):
         automargin=True,
         showlegend=False,
     )
+
+    annotations = []
+
+    for yd, xd in zip(y_data, x_data):
+        # labeling the y-axis
+        annotations.append(
+            dict(
+                xref='paper', yref='y',
+                x=0.14, y=yd,
+                xanchor='right',
+                text=str(yd),
+                font=dict(
+                    family='Arial', size=14,
+                    color='rgb(67, 67, 67)'
+                ),
+                showarrow=False, align='right'
+            )
+        )
+        # labeling the first percentage of each bar (x_axis)
+        annotations.append(
+            dict(
+                xref='x', yref='y',
+                x=xd[0] / 2, y=yd,
+                text=str(xd[0]) + '%',
+                font=dict(
+                    family='Arial', size=14,
+                    color='rgb(248, 248, 255)'
+                ),
+                showarrow=False
+            )
+        )
+        # labeling the first Likert scale (on the top)
+        if yd == y_data[-1]:
+            annotations.append(
+                dict(
+                    xref='x', yref='paper',
+                    x=xd[0] / 2, y=1.1,
+                    text=top_labels[0],
+                    font=dict(
+                        family='Arial', size=14,
+                        color='rgb(67, 67, 67)'
+                    ),
+                    showarrow=False
+                )
+            )
+        space = xd[0]
+        for i in range(1, len(xd)):
+            # labeling the rest of percentages for each bar (x_axis)
+            annotations.append(
+                dict(
+                    xref='x', yref='y',
+                    x=space + (xd[i]/2), y=yd,
+                    text=str(xd[i]) + '%',
+                    font=dict(
+                        family='Arial', size=14,
+                        color='rgb(248, 248, 255)'
+                    ),
+                    showarrow=False
+                )
+            )
+            # labeling the Likert scale
+            if yd == y_data[-1]:
+                annotations.append(
+                    dict(
+                        xref='x', yref='paper',
+                        x=space + (xd[i]/2), y=1.1,
+                        text=top_labels[i],
+                        font=dict(
+                            family='Arial', size=14,
+                            color='rgb(67, 67, 67)'
+                        ),
+                        showarrow=False
+                    )
+                )
+            space += xd[i]
+
+    fig.update_layout(annotations=annotations)
 
     tab1_content_ = html.Div([
         dbc.Row([dbc.Col(dcc.Graph(figure=fig), width=12)]),
