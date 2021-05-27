b49d073e6a042c96080021b0fac565eb70e6a7fb
Julien <peloton@lal.in2p3.fr>
2021-05-27 12:05:59 +0200

    colorbar

--- apps/plotting.py
+++ apps/plotting.py
@@ -303,7 +303,7 @@ def plot_classbar(pdf):
         'Ambiguous': 'brown',
         'Unknown': '#7f7f7f'
     }
-    colors__ = px.colors.sequential.Viridis
+    colors__ = px.colors.sequential.Portland
     colors = {
         'Early SN candidate': colors__[0],
         'Solar System MPC': colors__[1],
