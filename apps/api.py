99a066c31e0b79f38056866c4be9c4c5547d8ff6
Julien <peloton@lal.in2p3.fr>
2021-06-22 15:44:18 +0200

    Bring all Fink information

--- apps/api.py
+++ apps/api.py
@@ -1846,7 +1846,7 @@ def xmatch_user():
 
     # Final join
     join_df = pd.merge(
-        pdfs[['i:objectId', 'v:classification', 'v:separation_degree', idname]],
+        pdfs,
         df,
         on=idname
     )
