4c8cdd851e9b87b246209fca1c6674fd8dfc03f5
Julien <peloton@lal.in2p3.fr>
2021-06-23 11:06:08 +0200

    transparency

--- apps/explorer.py
+++ apps/explorer.py
@@ -1,3 +1,17 @@
+# Copyright 2020-2021 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
@@ -800,7 +814,7 @@ layout = html.Div(
     ],
     className='home',
     style={
-        'background-image': 'linear-gradient(rgba(255,255,255,0.8), rgba(255,255,255,0.8)), url(/assets/background.png)',
+        'background-image': 'linear-gradient(rgba(255,255,255,0.3), rgba(255,255,255,0.3)), url(/assets/background.png)',
         'background-size': 'contain'
     }
 )
