7ff7d34adf69a53800d0587bfb904611caef7d61
Julien <peloton@lal.in2p3.fr>
2021-07-05 10:54:13 +0200

    Add accordion skeleton

--- apps/summary.py
+++ apps/summary.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
