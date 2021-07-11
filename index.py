c75fda6915ef04e70ab5fbe38a057e7fb522d3e3
Julien <peloton@lal.in2p3.fr>
2021-07-11 22:24:37 +0200

    Use only science cutouts for quickview

--- index.py
+++ index.py
@@ -262,7 +262,7 @@ def carousel(nclick, data):
             slides_to_show=1,
             swipe_to_slide=True,
             autoplay=False,
-            speed=10,
+            speed=100,
             variable_width=False,
             center_mode=False
         )
