e2de5f6c89affdbff10a650d01e8f25530e8fac1
Julien <peloton@lal.in2p3.fr>
2021-07-19 22:48:26 +0200

    Update mulens layout

--- apps/cards.py
+++ apps/cards.py
@@ -354,10 +354,6 @@ def card_mulens_plot():
     card: dbc.Card
         Card with the microlensing fit drawn inside
     """
-    # card = dbc.Card(
-    #     dbc.CardBody(id='mulens_plot'),
-    #     className="mt-3"
-    # )
     card = html.Div(id='mulens_plot')
     return card
 
