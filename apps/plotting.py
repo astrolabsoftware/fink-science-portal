784575cba4cd4692501ea5cfd9ac901644fd68dc
Julien <peloton@lal.in2p3.fr>
2021-08-19 11:46:42 +0200

    Add a card with tracklet explanation

--- apps/plotting.py
+++ apps/plotting.py
@@ -2065,6 +2065,33 @@ def draw_tracklet_lightcurve(pathname: str, object_tracklet) -> dict:
         config={'displayModeBar': False}
     )
 
+    msg = """
+    Tracklets are discrete tracks (several spatially connected dots)
+    seen on one or more exposures. This is somehow similar to solar
+    system object, expect that these objects are probably human-made,
+    they are typically fast moving, and they seem to orbit
+    around the Earth (this type of orbit is also tight to
+    the detection method we use). The rotation period, in hour, is inferred from the
+    velocity estimate of the object assuming a circular orbit.
+
+    The velocity is computed by integrating the distance between subsequent measurements:
+
+    ```
+    v = sum_i[x(i+1) - x(i)] / dt, i=alert
+    ```
+
+    If the tracklet spans several exposures, we discard exposures with less than 5 alerts.
+    Discarded alerts are shown with cross markers, while alerts used to estimate the period are
+    shown with circle markers.
+    """
+    card_info = dbc.Card(
+        dbc.CardBody(
+            dcc.Markdown(msg)
+        ), style={
+            'backgroundColor': 'rgb(248, 248, 248, .7)'
+        }
+    )
+
     card = [
         dbc.Alert(
             "Tracklet ID: {} -- Inferred period: {:.1f} hours".format(
@@ -2076,7 +2103,8 @@ def draw_tracklet_lightcurve(pathname: str, object_tracklet) -> dict:
         dbc.Card(
             dbc.CardBody(graph),
             className="mt-3"
-        )
+        ),
+        card_info
     ]
     return card
 
--- apps/summary.py
+++ apps/summary.py
@@ -126,25 +126,13 @@ def tab5_content(pdf):
 def tab6_content(pdf):
     """ Tracklet tab
     """
-    msg = """
-    TBD
-    """
-    card_info = dbc.Card(
-        dbc.CardBody(
-            dcc.Markdown(msg)
-        ), style={
-            'backgroundColor': 'rgb(248, 248, 248, .7)'
-        }
-    )
-
     tab6_content_ = html.Div([
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        card_tracklet_lightcurve(),
                         card_tracklet_radec(),
-                        card_info
+                        card_tracklet_lightcurve()
                     ]
                 ),
             ]
