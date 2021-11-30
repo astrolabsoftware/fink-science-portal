975222bfaad16000de3cb7d48558513067ea2282
Julien <peloton@lal.in2p3.fr>
2021-11-30 10:15:49 +0100

    Reduce the number of columns to transfer

--- apps/statistics.py
+++ apps/statistics.py
@@ -36,10 +36,11 @@ def store_stat_query(name):
     # Query everything from this century
     name = 'ztf_20'
 
+    cols = 'key:key,basic:raw,basic:sci,basic:fields,basic:exposures'
     results = clientStats.scan(
         "",
         "key:key:{}".format(name),
-        "*",
+        cols,
         0,
         True,
         True
