724a76ec10143f042fb4bb61d3f0e121c4433f12
Julien <peloton@lal.in2p3.fr>
2022-01-05 14:49:26 +0100

    Update paths

--- bin/install_spark.sh
+++ bin/install_spark.sh
@@ -19,7 +19,11 @@ SPARK_VERSION=2.4.7
 
 wget https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop2.7.tgz
 tar -xf spark-${SPARK_VERSION}-bin-hadoop2.7.tgz
+rm spark-${SPARK_VERSION}-bin-hadoop2.7.tgz
+
 echo "export SPARK_HOME=$FSP_HOME/spark-${SPARK_VERSION}-bin-hadoop2.7" >> ~/.bash_profile
+export SPARK_HOME=$FSP_HOME/spark-${SPARK_VERSION}-bin-hadoop2.7
+
 echo "export PATH=$PATH:${SPARK_HOME}/bin:${SPARK_HOME}/sbin" >> ~/.bash_profile
 echo "spark.yarn.jars=${SPARK_HOME}/jars/*.jar" > ${SPARK_HOME}/conf/spark-defaults.conf
 echo "ARROW_PRE_0_15_IPC_FORMAT=1" > ${SPARK_HOME}/conf/spark-env.sh
