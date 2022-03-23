f38c443a4ba0fdbf90ebb6e2748c6f79fc6ace6a
Julien <peloton@lal.in2p3.fr>
2022-03-23 08:29:19 +0100

    reformmat explorer endpoint

--- apps/api/api.py
+++ apps/api/api.py
@@ -39,7 +39,7 @@ from apps.api.doc import api_doc_summary, api_doc_object, api_doc_explorer
 from apps.api.doc import api_doc_latests, api_doc_sso, api_doc_tracklets
 from apps.api.doc import api_doc_cutout, api_doc_xmatch, api_doc_bayestar, api_doc_stats
 
-from apps.api.utils import return_object_pdf
+from apps.api.utils import return_object_pdf, return_explorer_pdf
 
 import io
 import requests
@@ -518,19 +518,27 @@ def return_object(payload=None):
 def query_db_arguments():
     """ Obtain information about querying the Fink database
     """
-    return jsonify({'args': args_explorer})
+    if request.args is not None:
+        # POST from query URL
+        return query_db(payload=request.args)
+    else:
+        return jsonify({'args': args_explorer})
 
 @api_bp.route('/api/v1/explorer', methods=['POST'])
-def query_db():
+def query_db(payload=None):
     """ Query the Fink database
     """
-    if 'output-format' in request.json:
-        output_format = request.json['output-format']
+    # get payload from the JSON
+    if payload is None:
+        payload = request.json
+
+    if 'output-format' in payload:
+        output_format = payload['output-format']
     else:
         output_format = 'json'
 
     # Check the user specifies only one group
-    all_groups = [i['group'] for i in args_explorer if i['group'] is not None and i['name'] in request.json]
+    all_groups = [i['group'] for i in args_explorer if i['group'] is not None and i['name'] in payload]
     if len(np.unique(all_groups)) != 1:
         rep = {
             'status': 'error',
@@ -543,202 +551,14 @@ def query_db():
     required_args = [i['name'] for i in args_explorer if i['group'] == user_group]
     required = [i['required'] for i in args_explorer if i['group'] == user_group]
     for required_arg, required_ in zip(required_args, required):
-        if (required_arg not in request.json) and required_:
+        if (required_arg not in payload) and required_:
             rep = {
                 'status': 'error',
                 'text': "A value for `{}` is required for group {}. Use GET to check arguments.\n".format(required_arg, user_group)
             }
             return Response(str(rep), 400)
 
-    if user_group == 0:
-        # objectId search
-        to_evaluate = "key:key:{}".format(request.json['objectId'])
-
-        # Avoid a full scan
-        client.setLimit(1000)
-
-        results = client.scan(
-            "",
-            to_evaluate,
-            "*",
-            0, True, True
-        )
-        # reset the limit in case it has been changed above
-        client.setLimit(nlimit)
-
-        schema_client = client.schema()
-    if user_group == 1:
-        # Interpret user input
-        ra, dec = request.json['ra'], request.json['dec']
-        radius = request.json['radius']
-
-        if 'startdate_conesearch' in request.json:
-            startdate = request.json['startdate_conesearch']
-        else:
-            startdate = None
-        if 'window_days_conesearch' in request.json and request.json['window_days_conesearch'] is not None:
-            window_days = float(request.json['window_days_conesearch'])
-        else:
-            window_days = 1.0
-
-        if float(radius) > 18000.:
-            rep = {
-                'status': 'error',
-                'text': "`radius` cannot be bigger than 18,000 arcseconds (5 degrees).\n"
-            }
-            return Response(str(rep), 400)
-
-        try:
-            if 'h' in str(ra):
-                coord = SkyCoord(ra, dec, frame='icrs')
-            elif ':' in str(ra) or ' ' in str(ra):
-                coord = SkyCoord(ra, dec, frame='icrs', unit=(u.hourangle, u.deg))
-            else:
-                coord = SkyCoord(ra, dec, frame='icrs', unit='deg')
-        except ValueError as e:
-            rep = {
-                'status': 'error',
-                'text': e
-            }
-            return Response(str(rep), 400)
-
-        ra = coord.ra.deg
-        dec = coord.dec.deg
-        radius_deg = float(radius) / 3600.
-
-        # angle to vec conversion
-        vec = hp.ang2vec(np.pi / 2.0 - np.pi / 180.0 * dec, np.pi / 180.0 * ra)
-
-        # Send request
-        if float(radius) <= 30.:
-            nside = 131072
-            clientP_ = clientP131072
-        elif (float(radius) > 30.) & (float(radius) <= 1000.):
-            nside = 4096
-            clientP_ = clientP4096
-        else:
-            nside = 128
-            clientP_ = clientP128
-
-        pixs = hp.query_disc(
-            nside,
-            vec,
-            np.pi / 180 * radius_deg,
-            inclusive=True
-        )
-
-        # For the future: we could set clientP_.setRangeScan(True)
-        # and pass directly the time boundaries here instead of
-        # grouping by later.
-
-        # Filter by time - logic to be improved...
-        if startdate is not None:
-            if ':' in str(startdate):
-                jdstart = Time(startdate).jd
-            elif str(startdate).startswith('24'):
-                jdstart = Time(startdate, format='jd').jd
-            else:
-                jdstart = Time(startdate, format='mjd').jd
-            jdend = jdstart + window_days
-
-            clientP_.setRangeScan(True)
-            results = java.util.TreeMap()
-            for pix in pixs:
-                to_search = "key:key:{}_{},key:key:{}_{}".format(pix, jdstart, pix, jdend)
-                result = clientP_.scan(
-                    "",
-                    to_search,
-                    "*",
-                    0, True, True
-                )
-                results.putAll(result)
-            clientP_.setRangeScan(False)
-        else:
-            to_evaluate = ",".join(
-                [
-                    'key:key:{}'.format(i) for i in pixs
-                ]
-            )
-            # Get matches in the pixel index table
-            results = clientP_.scan(
-                "",
-                to_evaluate,
-                "*",
-                0, True, True
-            )
-
-        # extract objectId and times
-        objectids = [i[1]['i:objectId'] for i in results.items()]
-        times = [float(i[1]['key:key'].split('_')[1]) for i in results.items()]
-        pdf_ = pd.DataFrame({'oid': objectids, 'jd': times})
-
-        # Filter by time - logic to be improved...
-        if startdate is not None:
-            pdf_ = pdf_[(pdf_['jd'] >= jdstart) & (pdf_['jd'] < jdstart + window_days)]
-
-        # groupby and keep only the last alert per objectId
-        pdf_ = pdf_.loc[pdf_.groupby('oid')['jd'].idxmax()]
-
-        # Get data from the main table
-        results = java.util.TreeMap()
-        for oid, jd in zip(pdf_['oid'].values, pdf_['jd'].values):
-            to_evaluate = "key:key:{}_{}".format(oid, jd)
-
-            result = client.scan(
-                "",
-                to_evaluate,
-                "*",
-                0, True, True
-            )
-            results.putAll(result)
-        schema_client = client.schema()
-    elif user_group == 2:
-        if int(request.json['window']) > 180:
-            rep = {
-                'status': 'error',
-                'text': "`window` cannot be bigger than 180 minutes.\n"
-            }
-            return Response(str(rep), 400)
-        # Time to jd
-        jd_start = Time(request.json['startdate']).jd
-        jd_end = jd_start + TimeDelta(int(request.json['window']) * 60, format='sec').jd
-
-        # Send the request. RangeScan.
-        clientT.setRangeScan(True)
-        to_evaluate = "key:key:{},key:key:{}".format(jd_start, jd_end)
-        results = clientT.scan(
-            "",
-            to_evaluate,
-            "*",
-            0, True, True
-        )
-        schema_client = clientT.schema()
-
-    # reset the limit in case it has been changed above
-    client.setLimit(nlimit)
-
-    pdfs = format_hbase_output(
-        results,
-        schema_client,
-        group_alerts=True,
-        extract_color=False
-    )
-
-    # For conesearch, sort by distance
-    if (user_group == 1) and (len(pdfs) > 0):
-        sep = coord.separation(
-            SkyCoord(
-                pdfs['i:ra'],
-                pdfs['i:dec'],
-                unit='deg'
-            )
-        ).deg
-
-        pdfs['v:separation_degree'] = sep
-        pdfs = pdfs.sort_values('v:separation_degree', ascending=True)
-
-        mask = pdfs['v:separation_degree'] > radius_deg
-        pdfs = pdfs[~mask]
+    pdfs = return_explorer_pdf(payload)
 
     if output_format == 'json':
         return pdfs.to_json(orient='records')
--- apps/api/utils.py
+++ apps/api/utils.py
@@ -16,28 +16,31 @@ import java
 
 import pandas as pd
 import numpy as np
+import healpy as hp
+
+from astropy.coordinates import SkyCoord
+from astropy.time import Time, TimeDelta
 
 from app import client
 from app import clientU, clientUV
+from app import clientP128, clientP4096, clientP131072
+from app import clientT
 from app import nlimit
 
 from apps.utils import format_hbase_output
 from apps.utils import extract_cutouts
 
+from flask import Response
+
 def return_object_pdf(payload: dict) -> pd.DataFrame:
-    """ Make a query in HBase and format it in a Pandas dataframe
+    """ Extract data returned by HBase and format it in a Pandas dataframe
+
+    Data is from /api/v1/objects
 
     Parameters
     ----------
-    objectids: list
-        List of ZTF objectId
-    withupperlim: bool
-        If True, add upper limit measurements. Default is False.
-    withcutouts: bool
-        If True, add cutouts. Default is False.
-    cols: str
-        Comma-separated list of alert fields to transfer.
-        Default is all columns `cols='*'`
+    payload: dict
+        See https://fink-portal.org/api/v1/objects
 
     Return
     ----------
@@ -139,3 +142,206 @@ def return_object_pdf(payload: dict) -> pd.DataFrame:
             pdf = pdf_
 
     return pdf
+
+def return_explorer_pdf(payload: dict) -> pd.DataFrame:
+    """ Extract data returned by HBase and format it in a Pandas dataframe
+
+    Data is from /api/v1/explorer
+
+    Parameters
+    ----------
+    payload: dict
+        See https://fink-portal.org/api/v1/explorer
+
+    Return
+    ----------
+    out: pandas dataframe
+    """
+    if user_group == 0:
+        # objectId search
+        to_evaluate = "key:key:{}".format(payload['objectId'])
+
+        results = client.scan(
+            "",
+            to_evaluate,
+            "*",
+            0, True, True
+        )
+        # reset the limit in case it has been changed above
+        client.setLimit(nlimit)
+
+        schema_client = client.schema()
+    if user_group == 1:
+        # Interpret user input
+        ra, dec = payload['ra'], payload['dec']
+        radius = payload['radius']
+
+        if 'startdate_conesearch' in payload:
+            startdate = payload['startdate_conesearch']
+        else:
+            startdate = None
+        if 'window_days_conesearch' in payload and payload['window_days_conesearch'] is not None:
+            window_days = float(payload['window_days_conesearch'])
+        else:
+            window_days = 1.0
+
+        if float(radius) > 18000.:
+            rep = {
+                'status': 'error',
+                'text': "`radius` cannot be bigger than 18,000 arcseconds (5 degrees).\n"
+            }
+            return Response(str(rep), 400)
+
+        try:
+            if 'h' in str(ra):
+                coord = SkyCoord(ra, dec, frame='icrs')
+            elif ':' in str(ra) or ' ' in str(ra):
+                coord = SkyCoord(ra, dec, frame='icrs', unit=(u.hourangle, u.deg))
+            else:
+                coord = SkyCoord(ra, dec, frame='icrs', unit='deg')
+        except ValueError as e:
+            rep = {
+                'status': 'error',
+                'text': e
+            }
+            return Response(str(rep), 400)
+
+        ra = coord.ra.deg
+        dec = coord.dec.deg
+        radius_deg = float(radius) / 3600.
+
+        # angle to vec conversion
+        vec = hp.ang2vec(np.pi / 2.0 - np.pi / 180.0 * dec, np.pi / 180.0 * ra)
+
+        # Send request
+        if float(radius) <= 30.:
+            nside = 131072
+            clientP_ = clientP131072
+        elif (float(radius) > 30.) & (float(radius) <= 1000.):
+            nside = 4096
+            clientP_ = clientP4096
+        else:
+            nside = 128
+            clientP_ = clientP128
+
+        pixs = hp.query_disc(
+            nside,
+            vec,
+            np.pi / 180 * radius_deg,
+            inclusive=True
+        )
+
+        # For the future: we could set clientP_.setRangeScan(True)
+        # and pass directly the time boundaries here instead of
+        # grouping by later.
+
+        # Filter by time - logic to be improved...
+        if startdate is not None:
+            if ':' in str(startdate):
+                jdstart = Time(startdate).jd
+            elif str(startdate).startswith('24'):
+                jdstart = Time(startdate, format='jd').jd
+            else:
+                jdstart = Time(startdate, format='mjd').jd
+            jdend = jdstart + window_days
+
+            clientP_.setRangeScan(True)
+            results = java.util.TreeMap()
+            for pix in pixs:
+                to_search = "key:key:{}_{},key:key:{}_{}".format(pix, jdstart, pix, jdend)
+                result = clientP_.scan(
+                    "",
+                    to_search,
+                    "*",
+                    0, True, True
+                )
+                results.putAll(result)
+            clientP_.setRangeScan(False)
+        else:
+            to_evaluate = ",".join(
+                [
+                    'key:key:{}'.format(i) for i in pixs
+                ]
+            )
+            # Get matches in the pixel index table
+            results = clientP_.scan(
+                "",
+                to_evaluate,
+                "*",
+                0, True, True
+            )
+
+        # extract objectId and times
+        objectids = [i[1]['i:objectId'] for i in results.items()]
+        times = [float(i[1]['key:key'].split('_')[1]) for i in results.items()]
+        pdf_ = pd.DataFrame({'oid': objectids, 'jd': times})
+
+        # Filter by time - logic to be improved...
+        if startdate is not None:
+            pdf_ = pdf_[(pdf_['jd'] >= jdstart) & (pdf_['jd'] < jdstart + window_days)]
+
+        # groupby and keep only the last alert per objectId
+        pdf_ = pdf_.loc[pdf_.groupby('oid')['jd'].idxmax()]
+
+        # Get data from the main table
+        results = java.util.TreeMap()
+        for oid, jd in zip(pdf_['oid'].values, pdf_['jd'].values):
+            to_evaluate = "key:key:{}_{}".format(oid, jd)
+
+            result = client.scan(
+                "",
+                to_evaluate,
+                "*",
+                0, True, True
+            )
+            results.putAll(result)
+        schema_client = client.schema()
+    elif user_group == 2:
+        if int(payload['window']) > 180:
+            rep = {
+                'status': 'error',
+                'text': "`window` cannot be bigger than 180 minutes.\n"
+            }
+            return Response(str(rep), 400)
+        # Time to jd
+        jd_start = Time(payload['startdate']).jd
+        jd_end = jd_start + TimeDelta(int(payload['window']) * 60, format='sec').jd
+
+        # Send the request. RangeScan.
+        clientT.setRangeScan(True)
+        to_evaluate = "key:key:{},key:key:{}".format(jd_start, jd_end)
+        results = clientT.scan(
+            "",
+            to_evaluate,
+            "*",
+            0, True, True
+        )
+        schema_client = clientT.schema()
+
+    # reset the limit in case it has been changed above
+    client.setLimit(nlimit)
+
+    pdfs = format_hbase_output(
+        results,
+        schema_client,
+        group_alerts=True,
+        extract_color=False
+    )
+
+    # For conesearch, sort by distance
+    if (user_group == 1) and (len(pdfs) > 0):
+        sep = coord.separation(
+            SkyCoord(
+                pdfs['i:ra'],
+                pdfs['i:dec'],
+                unit='deg'
+            )
+        ).deg
+
+        pdfs['v:separation_degree'] = sep
+        pdfs = pdfs.sort_values('v:separation_degree', ascending=True)
+
+        mask = pdfs['v:separation_degree'] > radius_deg
+        pdfs = pdfs[~mask]
+
+    return pdfs
