ed51d6d11c07dc86f72d1cf4ff34c7aa7c2200d2
JulienPeloton <peloton@lal.in2p3.fr>
2022-07-22 12:36:08 +0200

    download

--- apps/cards.py
+++ apps/cards.py
@@ -743,22 +743,19 @@ def card_id1(pdf):
     classification = pdf['v:classification'].values[0]
 
     card = dmc.Paper(
-        dbc.Card(
-            [
-                html.H5("Last alert class: {}".format(classification), className="card-subtitle"),
-                dcc.Markdown(
-                    """
-                    ```python
-                    Discovery date: {}
-                    Last detection: {}
-                    Number of detections: {}
-                    ```
-                    """.format(
-                        discovery_date, date_end, ndet)
-                ),
-            ],
-            className="mt-3", body=True
-        ), radius='xl', p='md', shadow='xl', withBorder=True
+        [
+            html.H5("Last alert class: {}".format(classification), className="card-subtitle"),
+            dcc.Markdown(
+                """
+                ```python
+                Discovery date: {}
+                Last detection: {}
+                Number of detections: {}
+                ```
+                """.format(
+                    discovery_date, date_end, ndet)
+            ),
+        ], radius='xl', p='md', shadow='xl', withBorder=True
     )
     return card
 
