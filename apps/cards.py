4eecaca079d888250a4d54421e5bcaaa5683f754
JulienPeloton <peloton@lal.in2p3.fr>
2022-07-22 12:50:34 +0200

    download

--- apps/cards.py
+++ apps/cards.py
@@ -747,8 +747,8 @@ def card_id1(pdf):
         [
             dbc.Row(
                 [
-                    dbc.Col(dmc.Avatar(src="/assets/Fink_SecondaryLogo_WEB.png"), width=4),
-                    dbc.Col(dmc.Title(objectid, order=1, style={'color': '#15284F'}), width=8),
+                    dbc.Col(dmc.Avatar(src="/assets/Fink_SecondaryLogo_WEB.png"), width=2),
+                    dbc.Col(dmc.Title(objectid, order=1, style={'color': '#15284F'}), width=10),
                 ], justify='start'
             ),
             dcc.Markdown(
