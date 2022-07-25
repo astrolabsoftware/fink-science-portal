7dfefd085d7e437da88e22b24b4fb3a1ac9da9db
JulienPeloton <peloton@lal.in2p3.fr>
2022-07-25 11:36:33 +0200

    Fill card

--- apps/cards.py
+++ apps/cards.py
@@ -773,11 +773,11 @@ def card_id1(object_data, object_uppervalid, object_upper):
     objectid = pdf['i:objectId'].values[0]
     date_end = pdf['v:lastdate'].values[0]
     discovery_date = pdf['v:lastdate'].values[-1]
-    candid = pdf['i:candid'].values
+    jds = pdf['i:jd'].values
     ndet = len(pdf)
 
     pdf_upper_valid = pd.read_json(object_uppervalid)
-    mask = pdf_upper_valid['i:candid'].apply(lambda x: x not in candid)
+    mask = pdf_upper_valid['i:jd'].apply(lambda x: x not in jds)
     nupper_valid = len(pdf_upper_valid[mask])
     pdf_upper = pd.read_json(object_upper)
     nupper = len(pdf_upper)
