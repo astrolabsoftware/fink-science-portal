0e84795eb50153a017ffc7a2ce32b06718d346cc
JulienPeloton <peloton@lal.in2p3.fr>
2022-07-25 22:15:10 +0200

    Loading

--- index.py
+++ index.py
@@ -1262,7 +1262,7 @@ def display_page(pathname, is_mobile):
                     noresults_toast
                 ], id='trash', fluid=True, style={'width': width}
             ),
-            dmc.LoadingOverlay(dbc.Container(id='results')),
+            dmc.LoadingOverlay(dbc.Container(id='results'), loaderProps={"variant": "oval", "color": "blue", "size": "xl"},),
             dbc.Input(id='validate_results', style={'display': 'none'}),
         ],
         className='home',
