d0e556173102c56b89aeaf7cce38a6ab78635349
JulienPeloton <peloton@lal.in2p3.fr>
2022-07-26 13:30:16 +0200

    refactoring

--- apps/plotting.py
+++ apps/plotting.py
@@ -383,6 +383,176 @@ layout_tracklet_lightcurve = dict(
 )
 
 @app.callback(
+    [
+        Output('variable_plot', 'children'),
+        Output("submit_variable", "children"),
+    ],
+    [
+        Input('nterms_base', 'value'),
+        Input('nterms_band', 'value'),
+        Input('manual_period', 'value'),
+        Input('submit_variable', 'n_clicks'),
+        Input('object-data', 'children'),
+    ],
+    prevent_initial_call=True
+)
+def plot_variable_star(nterms_base, nterms_band, manual_period, n_clicks, object_data):
+    """ Fit for the period of a star using gatspy
+
+    See https://zenodo.org/record/47887
+    See https://ui.adsabs.harvard.edu/abs/2015ApJ...812...18V/abstract
+
+    TODO: clean me
+    """
+    if type(nterms_base) not in [int]:
+        return {'data': [], "layout": layout_phase}
+    if type(nterms_band) not in [int]:
+        return {'data': [], "layout": layout_phase}
+    if manual_period is not None and type(manual_period) not in [int, float]:
+        return {'data': [], "layout": layout_phase}
+
+    if n_clicks is not None:
+        pdf_ = pd.read_json(object_data)
+        cols = [
+            'i:jd', 'i:magpsf', 'i:sigmapsf', 'i:fid',
+            'i:magnr', 'i:sigmagnr', 'i:magzpsci', 'i:isdiffpos', 'i:objectId'
+        ]
+        pdf = pdf_.loc[:, cols]
+        pdf['i:fid'] = pdf['i:fid'].astype(str)
+        pdf = pdf.sort_values('i:jd', ascending=False)
+
+        mag_dc, err_dc = np.transpose(
+            [
+                dc_mag(*args) for args in zip(
+                    pdf['i:fid'].astype(int).values,
+                    pdf['i:magpsf'].astype(float).values,
+                    pdf['i:sigmapsf'].astype(float).values,
+                    pdf['i:magnr'].astype(float).values,
+                    pdf['i:sigmagnr'].astype(float).values,
+                    pdf['i:magzpsci'].astype(float).values,
+                    pdf['i:isdiffpos'].values
+                )
+            ]
+        )
+
+        jd = pdf['i:jd']
+        fit_period = False if manual_period is not None else True
+        model = periodic.LombScargleMultiband(
+            Nterms_base=int(nterms_base),
+            Nterms_band=int(nterms_band),
+            fit_period=fit_period
+        )
+
+        # Not sure about that...
+        model.optimizer.period_range = (0.1, 1.2)
+        model.optimizer.quiet = True
+
+        model.fit(
+            jd.astype(float),
+            mag_dc,
+            err_dc,
+            pdf['i:fid'].astype(int)
+        )
+
+        if fit_period:
+            period = model.best_period
+        else:
+            period = manual_period
+
+        phase = jd.astype(float).values % period
+        tfit = np.linspace(0, period, 100)
+
+        layout_phase_ = copy.deepcopy(layout_phase)
+        layout_phase_['title']['text'] = 'Period: {} days - score: {:.2f}'.format(period, model.score(period))
+
+        if '1' in np.unique(pdf['i:fid'].values):
+            plot_filt1 = {
+                'x': phase[pdf['i:fid'] == '1'],
+                'y': mag_dc[pdf['i:fid'] == '1'],
+                'error_y': {
+                    'type': 'data',
+                    'array': err_dc[pdf['i:fid'] == '1'],
+                    'visible': True,
+                    'color': COLORS_ZTF[0]
+                },
+                'mode': 'markers',
+                'name': 'g band',
+                'text': phase[pdf['i:fid'] == '1'],
+                'marker': {
+                    'size': 12,
+                    'color': COLORS_ZTF[0],
+                    'symbol': 'o'}
+            }
+            fit_filt1 = {
+                'x': tfit,
+                'y': model.predict(tfit, period=period, filts=1),
+                'mode': 'lines',
+                'name': 'fit g band',
+                'showlegend': False,
+                'line': {
+                    'color': COLORS_ZTF[0],
+                }
+            }
+        else:
+            plot_filt1 = {}
+            fit_filt1 = {}
+
+        if '2' in np.unique(pdf['i:fid'].values):
+            plot_filt2 = {
+                'x': phase[pdf['i:fid'] == '2'],
+                'y': mag_dc[pdf['i:fid'] == '2'],
+                'error_y': {
+                    'type': 'data',
+                    'array': err_dc[pdf['i:fid'] == '2'],
+                    'visible': True,
+                    'color': COLORS_ZTF[1]
+                },
+                'mode': 'markers',
+                'name': 'r band',
+                'text': phase[pdf['i:fid'] == '2'],
+                'marker': {
+                    'size': 12,
+                    'color': COLORS_ZTF[1],
+                    'symbol': 'o'}
+            }
+            fit_filt2 = {
+                'x': tfit,
+                'y': model.predict(tfit, period=period, filts=2),
+                'mode': 'lines',
+                'name': 'fit r band',
+                'showlegend': False,
+                'line': {
+                    'color': COLORS_ZTF[1],
+                }
+            }
+        else:
+            plot_filt2 = {}
+            fit_filt2 = {}
+
+        figure = {
+            'data': [
+                plot_filt1,
+                fit_filt1,
+                plot_filt2,
+                fit_filt2
+            ],
+            "layout": layout_phase_
+        }
+        graph = dcc.Graph(
+            figure=figure,
+            style={
+                'width': '100%',
+                'height': '25pc'
+            },
+            config={'displayModeBar': False}
+        )
+
+        return graph, no_update
+
+    # quite referentially opaque...
+    return "", no_update
+
+@app.callback(
     Output('classbar', 'figure'),
     [
         Input('object-data', 'children'),
