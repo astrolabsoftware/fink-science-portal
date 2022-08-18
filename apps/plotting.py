8c275b66c0719d0139425b1005d6769fc69c5ab4
JulienPeloton <peloton@lal.in2p3.fr>
2022-08-18 14:45:58 +0200

    style

--- apps/plotting.py
+++ apps/plotting.py
@@ -1432,19 +1432,22 @@ def draw_cutouts_modal(object_data, date_modal_select):
     for kind in ['science', 'template', 'difference']:
         try:
             data = extract_cutout(object_data, date_modal_select, kind=kind)
-            figs.append(draw_cutout(data, kind, modal=True))
+            figs.append(
+                dmc.Tooltip(
+                    children=draw_cutout(data, kind, modal=True),
+                    wrapLines=True,
+                    width=220,
+                    withArrow=True,
+                    transition="fade",
+                    transitionDuration=200,
+                    label=kind,
+                    zIndex=10000001
+                )
+            )
         except OSError:
             data = dcc.Markdown("Load fail, refresh the page")
             figs.append(data)
-    return dmc.Tooltip(
-        children=figs,
-        wrapLines=True,
-        width=220,
-        withArrow=True,
-        transition="fade",
-        transitionDuration=200,
-        label=kind
-    )
+    return figs
 
 @app.callback(
     Output("stamps_mobile", "children"),
