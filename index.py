9c6af9086388766036998d566c1186f9abfc142c
JulienPeloton <peloton@lal.in2p3.fr>
2022-08-18 14:18:42 +0200

    style

--- index.py
+++ index.py
@@ -171,27 +171,11 @@ modal = html.Div(
             [
                 dbc.ModalHeader(dbc.ModalTitle("Fink Science Portal"), close_button=True),
                 dbc.ModalBody(dcc.Markdown(message_help)),
-                # dbc.ModalFooter(
-                #     dbc.Button("Close", id="close", className="ml-auto")
-                # ),
             ],
             id="modal", scrollable=True
         ),
     ]
 )
-
-@app.callback(
-    Output("modal", "is_open"),
-    [Input("open", "n_clicks"), Input("close", "n_clicks")],
-    [State("modal", "is_open")],
-)
-def toggle_modal(n1, n2, is_open):
-    """ Callback for the modal (open/close)
-    """
-    if n1 or n2:
-        return not is_open
-    return is_open
-
 
 fink_search_bar = dbc.InputGroup(
     [
