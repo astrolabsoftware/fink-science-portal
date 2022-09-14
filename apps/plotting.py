99edf757065cd035c9ae5c288772ef1c30f72218
JulienPeloton <peloton@lal.in2p3.fr>
2022-09-14 22:29:56 +0200

    Add residual plot

--- apps/plotting.py
+++ apps/plotting.py
@@ -3362,3 +3362,90 @@ def fields_exposures(pathname, dropdown_days):
     )
 
     return card
+
+@app.callback(
+    Output('coordinates', 'children'),
+    [
+        Input('object-data', 'children'),
+    ])
+def draw_alert_astrometry(object_data) -> dict:
+    """ Draw SSO object astrometry, that is difference position wrt ephemerides
+    from the miriade IMCCE service.
+
+    Parameters
+    ----------
+    pathname: str
+        Pathname of the current webpage (should be /ZTF19...).
+
+    Returns
+    ----------
+    figure: dict
+    """
+    pdf = pd.read_json(object_data)
+
+    mean_ra = np.mean(pdf['i:ra'])
+    mean_dec = np.mean(pdf['i:dec'])
+
+    deltaRAcosDEC = (pdf['i:ra'] - mean_ra) * np.cos(np.radians(pdf['i:dec'])) * 3600
+    deltaDEC = (pdf['i:dec'] - mean_dec) * 3600
+
+    hovertemplate = r"""
+    <b>objectId</b>: %{customdata[0]}<br>
+    <b>%{yaxis.title.text}</b>: %{y:.2f}<br>
+    <b>%{xaxis.title.text}</b>: %{x:.2f}<br>
+    <b>mjd</b>: %{customdata[1]}
+    <extra></extra>
+    """
+    diff_g = {
+        'x': deltaRAcosDEC[pdf['i:fid'] == 1],
+        'y': deltaDEC[pdf['i:fid'] == 1],
+        'mode': 'markers',
+        'name': 'g band',
+        'customdata': list(
+            zip(
+                pdf['i:objectId'][pdf['i:fid'] == 1],
+                pdf['i:jd'][pdf['i:fid'] == 1].apply(lambda x: float(x) - 2400000.5),
+            )
+        ),
+        'hovertemplate': hovertemplate,
+        'marker': {
+            'size': 6,
+            'color': COLORS_ZTF[0],
+            'symbol': 'o'}
+    }
+
+    diff_r = {
+        'x': deltaRAcosDEC[pdf['i:fid'] == 2],
+        'y': deltaDEC[pdf['i:fid'] == 2],
+        'mode': 'markers',
+        'name': 'r band',
+        'customdata': list(
+            zip(
+                pdf['i:objectId'][pdf['i:fid'] == 2],
+                pdf['i:jd'][pdf['i:fid'] == 2].apply(lambda x: float(x) - 2400000.5),
+            )
+        ),
+        'hovertemplate': hovertemplate,
+        'marker': {
+            'size': 6,
+            'color': COLORS_ZTF[1],
+            'symbol': 'o'}
+    }
+
+    figure = {
+        'data': [
+            diff_g,
+            diff_r,
+        ],
+        "layout": layout_sso_astrometry
+    }
+    graph = dcc.Graph(
+        figure=figure,
+        style={
+            'width': '100%',
+            'height': '25pc'
+        },
+        config={'displayModeBar': False}
+    )
+    card = dmc.Paper(graph, radius='xl', p='md', shadow='xl', withBorder=True)
+    return card
