b9c61799fb54db70b34f259979c764645732b98f
JulienPeloton <peloton@lal.in2p3.fr>
2022-10-03 14:53:44 +0200

    Title

--- apps/plotting.py
+++ apps/plotting.py
@@ -2485,7 +2485,7 @@ def draw_sso_phasecurve(pathname: str, switch_band: str, switch_func: str, objec
             }
         )
 
-    layout_sso_phasecurve['title']['text'] = '$chi^2_{{red}}$: {:.2f}'.format(chisq_red)
+    layout_sso_phasecurve['title']['text'] = r'$chi^2_{red}$: {:.2f}'.format(chisq_red)
     figure = {
         'data': figs,
         "layout": layout_sso_phasecurve
