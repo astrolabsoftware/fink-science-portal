c958b8d63dce3f1ebb3507ce9a4f345e798fa072
JulienPeloton <peloton@lal.in2p3.fr>
2022-10-03 13:16:58 +0200

    Typo

--- apps/plotting.py
+++ apps/plotting.py
@@ -2393,7 +2393,7 @@ def draw_sso_phasecurve(pathname: str, switch_band: str, switch_func: str, objec
             figs.append(
                 {
                     'x': pdf.loc[cond, 'Phase'].values,
-                    'y': fitfunc(x, *popt),
+                    'y': fitfunc(x[cond], *popt),
                     'mode': 'lines',
                     'name': 'fit {:}'.format(filters[f]),
                     'showlegend': False,
