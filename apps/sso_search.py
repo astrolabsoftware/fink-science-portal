706ab9d75e4a1739c43aa3a5194de1e49cfc4ac9
JulienPeloton <peloton@lal.in2p3.fr>
2022-11-10 23:59:00 +0100

    search

--- apps/sso_search.py
+++ apps/sso_search.py
@@ -45,6 +45,65 @@ from astropy.time import Time, TimeDelta
 from astropy.coordinates import name_resolve
 import astropy.utils as autils
 
+message_help = """
+Search for Solar System Objects in the Fink database.
+The numbers or designations are taken from the MPC archive.
+When searching for a particular asteroid or comet, it is best to use the IAU number,
+as in 8467 for asteroid "8467 Benoitcarry". You can also try for numbered comet (e.g. 10P),
+or interstellar object (none so far...). If the number does not yet exist, you can search for designation.
+Here are some examples of valid queries:
+
+* Asteroids by number (default)
+  * Asteroids (Main Belt): 8467, 1922
+  * Asteroids (Hungarians): 18582, 77799
+  * Asteroids (Jupiter Trojans): 4501, 1583
+  * Asteroids (Mars Crossers): 302530
+* Asteroids by designation (if number does not exist yet)
+  * 2010JO69, 2017AD19, 2012XK111
+* Comets by number (default)
+  * 10P, 249P, 124P
+* Comets by designation (if number does no exist yet)
+  * C/2020V2, C/2020R2
+
+Note for designation, you can also use space (2010 JO69 or C/2020 V2).
+
+"""
+
+modal = html.Div(
+    [
+        dbc.Button(
+            "Help",
+            id="open",
+            color='light',
+            outline=True,
+            style={
+                "border": "0px black solid",
+                'background': 'rgba(255, 255, 255, 0.0)',
+                'color': 'grey'
+            }
+        ),
+        dbc.Modal(
+            [
+                dbc.ModalHeader(dbc.ModalTitle("Solar System Objects (SSO)"), close_button=True),
+                dbc.ModalBody(dcc.Markdown(message_help)),
+            ],
+            id="modal2", scrollable=True
+        ),
+    ]
+)
+
+@app.callback(
+    Output("modal2", "is_open"),
+    [Input("open", "n_clicks")],
+    [State("modal2", "is_open")],
+)
+def toggle_modal(n1, is_open):
+    """ Callback for the modal (open/close)
+    """
+    if n1:
+        return not is_open
+    return is_open
+
 @app.callback(
     Output('selectSSO', 'options'),
     Input('selectSSO', 'search_value'),
@@ -85,7 +144,8 @@ fink_search_bar_sso = dbc.InputGroup(
             options=[],
             placeholder='Enter first letters or numbers of a SSO (e.g. cer)',
             style={"border": "0px black solid", 'background': 'rgba(255, 255, 255, 0.0)', 'color': 'grey', 'width': '100%'},
-        )
+        ),
+        modal
     ], style={"border": "0.5px grey solid", 'background': 'rgba(255, 255, 255, .75)'}, className='rcorners2'
 )
 
