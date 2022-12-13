1b4a35d6fd25e4edb5e77a890e39611e908dc2c1
JulienPeloton <peloton@lal.in2p3.fr>
2022-12-13 21:27:48 +0100

    Update query

--- apps/query_cluster.py
+++ apps/query_cluster.py
@@ -36,16 +36,14 @@ tns_types = sorted(tns_types, key=lambda s: s.lower())
     Output("timeline_data_transfer", "children"),
     [
         Input('trans_datasource', 'value'),
-        Input('filter_tab', 'children'),
-        Input('content_tab', 'children'),
-        Input('summary_tab', 'children')
+        Input('date-range-picker', 'children'),
     ]
 )
-def timeline_data_transfer(trans_datasource, date_range_picker, trans_content, summary_content):
+def timeline_data_transfer(trans_datasource, date_range_picker):
     """
     """
     active_ = np.where(
-        np.array([trans_datasource, date_range_picker, trans_content, summary_content]) != None
+        np.array([trans_datasource, date_range_picker]) != None
     )[0]
     tmp = len(active_) - 1
     nsteps = 0 if tmp < 0 else tmp
@@ -111,147 +109,148 @@ def timeline_data_transfer(trans_datasource, date_range_picker, trans_content, s
 
     return timeline
 
-@app.callback(
-    Output("filter_tab", "children"),
-    [
-        Input('trans_datasource', 'value'),
-    ], prevent_initial_call=True
-)
-def filter_tab(trans_datasource):
+def filter_tab():
     """ Section containing filtering options
     """
-    if trans_datasource == 'ztf':
-        options = html.Div(
-            [
-                dmc.DateRangePicker(
-                    id="date-range-picker",
-                    label="Date Range",
-                    description="Pick up start and stop dates (included), with a maximum of 14 nights allowed.",
-                    minDate=date(2019, 11, 1),
-                    maxDate=date.today(),
-                    value=None,
-                    style={"width": 500},
-                    hideOutsideDates=True,
-                    amountOfMonths=2,
-                    allowSingleDateInRange=True,
-                    required=True
-                ),
-                dmc.Space(h=10),
-                dmc.MultiSelect(
-                    label="Alert class",
-                    description="Select all classes you like!",
-                    placeholder="start typing...",
-                    id="class_select",
-                    value=None,
-                    data = [
-                        {'label': 'All classes', 'value': 'allclasses'},
-                        {'label': 'Unknown', 'value': 'Unknown'},
-                        {'label': '(Fink) Early Supernova Ia candidates', 'value': 'Early SN Ia candidate'},
-                        {'label': '(Fink) Supernova candidates', 'value': 'SN candidate'},
-                        {'label': '(Fink) Kilonova candidates', 'value': 'Kilonova candidate'},
-                        {'label': '(Fink) Microlensing candidates', 'value': 'Microlensing candidate'},
-                        {'label': '(Fink) Solar System (MPC)', 'value': 'Solar System MPC'},
-                        {'label': '(Fink) Solar System (candidates)', 'value': 'Solar System candidate'},
-                        {'label': '(Fink) Tracklet (space debris & satellite glints)', 'value': 'Tracklet'},
-                        {'label': '(Fink) Ambiguous', 'value': 'Ambiguous'},
-                        *[{'label': '(TNS) ' + simtype, 'value': '(TNS) ' + simtype} for simtype in tns_types],
-                        *[{'label': '(SIMBAD) ' + simtype, 'value': '(SIMBAD) ' + simtype} for simtype in simbad_types]
-                    ],
-                    searchable=True,
-                    style={"width": 500},
-                ),
-                dmc.Space(h=10),
-                dmc.Textarea(
-                    id="extra_cond",
-                    label="Extra conditions",
-                    description=[
-                        "One condition per line (SQL syntax), ending with semi-colon. See ",
-                        dmc.Anchor("here", href="https://fink-portal.org/api/v1/columns", size="xs"),
-                        " for field description.",
-                    ],
-                    placeholder="e.g. magpsf > 19.5;",
-                    style={"width": 500},
-                    autosize=True,
-                    minRows=2,              ),
-            ]
-        )
-        tab = html.Div(
-            [
-                dmc.Space(h=10),
-                dmc.Divider(variant="solid", label='Filters'),
-                options,
-            ]
-        )
-        return tab
-    elif trans_datasource == 'elasticc':
-        return html.Div("Under construction...")
-    else:
-        return html.Div()
+    options = html.Div(
+        [
+            dmc.DateRangePicker(
+                id="date-range-picker",
+                label="Date Range",
+                description="Pick up start and stop dates (included), with a maximum of 14 nights allowed.",
+                minDate=date(2019, 11, 1),
+                maxDate=date.today(),
+                value=None,
+                style={"width": 500},
+                hideOutsideDates=True,
+                amountOfMonths=2,
+                allowSingleDateInRange=True,
+                required=True
+            ),
+            dmc.Space(h=10),
+            dmc.MultiSelect(
+                label="Alert class",
+                description="Select all classes you like!",
+                placeholder="start typing...",
+                id="class_select",
+                value=None,
+                data = [
+                    {'label': 'All classes', 'value': 'allclasses'},
+                    {'label': 'Unknown', 'value': 'Unknown'},
+                    {'label': '(Fink) Early Supernova Ia candidates', 'value': 'Early SN Ia candidate'},
+                    {'label': '(Fink) Supernova candidates', 'value': 'SN candidate'},
+                    {'label': '(Fink) Kilonova candidates', 'value': 'Kilonova candidate'},
+                    {'label': '(Fink) Microlensing candidates', 'value': 'Microlensing candidate'},
+                    {'label': '(Fink) Solar System (MPC)', 'value': 'Solar System MPC'},
+                    {'label': '(Fink) Solar System (candidates)', 'value': 'Solar System candidate'},
+                    {'label': '(Fink) Tracklet (space debris & satellite glints)', 'value': 'Tracklet'},
+                    {'label': '(Fink) Ambiguous', 'value': 'Ambiguous'},
+                    *[{'label': '(TNS) ' + simtype, 'value': '(TNS) ' + simtype} for simtype in tns_types],
+                    *[{'label': '(SIMBAD) ' + simtype, 'value': '(SIMBAD) ' + simtype} for simtype in simbad_types]
+                ],
+                searchable=True,
+                style={"width": 500},
+            ),
+            dmc.Space(h=10),
+            dmc.Textarea(
+                id="extra_cond",
+                label="Extra conditions",
+                description=[
+                    "One condition per line (SQL syntax), ending with semi-colon. See ",
+                    dmc.Anchor("here", href="https://fink-portal.org/api/v1/columns", size="xs"),
+                    " for field description.",
+                ],
+                placeholder="e.g. magpsf > 19.5;",
+                style={"width": 500},
+                autosize=True,
+                minRows=2,              ),
+        ]
+    )
+    tab = html.Div(
+        [
+            dmc.Space(h=10),
+            dmc.Divider(variant="solid", label='Filters'),
+            options,
+        ], id='filter_tab', style={'display': 'none'}
+    )
+    return tab
 
 @app.callback(
-    Output("content_tab", "children"),
+    Output("filter_tab", "style"),
     [
-        Input('date-range-picker', 'value')
+        Input('trans_datasource', 'value')
     ], prevent_initial_call=True
 )
-def content_tab(date_range_picker):
-    """ Section containing filtering options
-    """
-    if date_range_picker is not None:
-        tab = html.Div(
-            [
-                dmc.Space(h=10),
-                dmc.Divider(variant="solid", label='Alert content'),
-                dmc.RadioGroup(
-                    id="trans_content",
-                    data=[
-                        {"value": "Full packet", "label": "Full packet"},
-                        {"value": "Lightcurve", "label": "Lightcurve"},
-                        {"value": "Cutouts", "label": "Cutouts"},
-                    ],
-                    value=None,
-                    label="Choose the content you want to retrieve",
-                    size="sm",
-                    color='orange'
-                ),
-            ]
-        )
-        return tab
-    else:
-        PreventUpdate
-
-@app.callback(
-    Output("summary_tab", "children"),
-    [
-        Input('trans_content', 'value'),
-    ],
-    [
-        State('trans_datasource', 'value'),
-        State('date-range-picker', 'value'),
-        State('class_select', 'value'),
-        State('extra_cond', 'value'),
-    ],
-    prevent_initial_call=True
-)
-def summary_tab(trans_content, trans_datasource, date_range_picker, class_select, extra_cond):
-    """ Section containing summary
-    """
-    if trans_content is None:
+def display_filter_tab(trans_datasource):
+    if trans_datasource is None:
         PreventUpdate
     else:
-        tab = html.Div(
-            [
-                dmc.Space(h=10),
-                dmc.Divider(variant="solid", label='Summary'),
-                dmc.Text('Source: {}'.format(trans_datasource)),
-                dmc.Text('Dates: {} - {}'.format(*date_range_picker)),
-                dmc.Text('Classe(s): {}'.format(class_select)),
-                dmc.Text('Conditions: {}'.format(extra_cond)),
-                dmc.Text('Content: {}'.format(trans_content)),
-            ]
+        return {}
 
-        )
-        return tab
+# @app.callback(
+#     Output("content_tab", "children"),
+#     [
+#         Input('date-range-picker', 'value')
+#     ], prevent_initial_call=True
+# )
+# def content_tab(date_range_picker):
+#     """ Section containing filtering options
+#     """
+#     if date_range_picker is not None:
+#         tab = html.Div(
+#             [
+#                 dmc.Space(h=10),
+#                 dmc.Divider(variant="solid", label='Alert content'),
+#                 dmc.RadioGroup(
+#                     id="trans_content",
+#                     data=[
+#                         {"value": "Full packet", "label": "Full packet"},
+#                         {"value": "Lightcurve", "label": "Lightcurve"},
+#                         {"value": "Cutouts", "label": "Cutouts"},
+#                     ],
+#                     value=None,
+#                     label="Choose the content you want to retrieve",
+#                     size="sm",
+#                     color='orange'
+#                 ),
+#             ]
+#         )
+#         return tab
+#     else:
+#         PreventUpdate
+
+# @app.callback(
+#     Output("summary_tab", "children"),
+#     [
+#         Input('trans_content', 'value'),
+#     ],
+#     [
+#         State('trans_datasource', 'value'),
+#         State('date-range-picker', 'value'),
+#         State('class_select', 'value'),
+#         State('extra_cond', 'value'),
+#     ],
+#     prevent_initial_call=True
+# )
+# def summary_tab(trans_content, trans_datasource, date_range_picker, class_select, extra_cond):
+#     """ Section containing summary
+#     """
+#     if trans_content is None:
+#         PreventUpdate
+#     else:
+#         tab = html.Div(
+#             [
+#                 dmc.Space(h=10),
+#                 dmc.Divider(variant="solid", label='Summary'),
+#                 dmc.Text('Source: {}'.format(trans_datasource)),
+#                 dmc.Text('Dates: {} - {}'.format(*date_range_picker)),
+#                 dmc.Text('Classe(s): {}'.format(class_select)),
+#                 dmc.Text('Conditions: {}'.format(extra_cond)),
+#                 dmc.Text('Content: {}'.format(trans_content)),
+#             ]
+
+#         )
+#         return tab
 
 def query_builder():
     """ Build iteratively the query based on user inputs.
@@ -285,6 +284,7 @@ def layout(is_mobile):
     """ Layout for the data transfer service
     """
     qb = query_builder()
+    ft = filter_tab()
     layout_ = html.Div(
         [
             html.Br(),
@@ -302,9 +302,7 @@ def layout(is_mobile):
                     dbc.Col(
                         [
                             qb,
-                            html.Div(None, id='filter_tab'),
-                            html.Div(None, id='content_tab'),
-                            html.Div(None, id='summary_tab'),
+                            ft,
                         ],
                         width=8)
                 ],
