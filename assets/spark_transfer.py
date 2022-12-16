5aa95513a4a58499d173d25b8010f44712dcb859
JulienPeloton <peloton@lal.in2p3.fr>
2022-12-16 10:28:08 +0100

    Upload Spark script

---
+++ assets/spark_transfer.py
@@ -0,0 +1,351 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.column import Column, _to_java_column
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import struct, lit
+from pyspark import SparkContext
+from time import time
+import pandas as pd
+import io
+import os
+import glob
+import shutil
+import json
+import fastavro
+import sys
+import subprocess
+import argparse
+import requests
+
+def to_avro(dfcol: Column) -> Column:
+    """Serialize the structured data of a DataFrame column into
+    avro data (binary).
+
+    Note:
+    Since Pyspark does not have a function to convert a column to and from
+    avro data, this is a wrapper around the scala function 'to_avro'.
+    Just like the function above, to be able to use this you need to have
+    the package org.apache.spark:spark-avro_2.11:2.x.y in the classpath.
+
+    Parameters
+    ----------
+    dfcol: Column
+        A DataFrame Column with Structured data
+
+    Returns
+    ----------
+    out: Column
+        DataFrame Column encoded into avro data (binary).
+        This is what is required to publish to Kafka Server for distribution.
+
+    Examples
+    ----------
+    >>> from pyspark.sql.functions import col, struct
+    >>> avro_example_schema = '''
+    ... {
+    ...     "type" : "record",
+    ...     "name" : "struct",
+    ...     "fields" : [
+    ...             {"name" : "col1", "type" : "long"},
+    ...             {"name" : "col2", "type" : "string"}
+    ...     ]
+    ... }'''
+    >>> df = spark.range(5)
+    >>> df = df.select(struct("id",\
+                 col("id").cast("string").alias("id2"))\
+                 .alias("struct"))
+    >>> avro_df = df.select(to_avro(col("struct")).alias("avro"))
+    """
+    sc = SparkContext._active_spark_context
+    avro = sc._jvm.org.apache.spark.sql.avro
+    f = getattr(getattr(avro, "package$"), "MODULE$").to_avro
+    return Column(f(_to_java_column(dfcol)))
+
+def save_avro_schema(df: DataFrame, schema_path: str):
+    """Writes the avro schema to a file at schema_path
+
+    This routine checks if an avro schema file exist at the given path
+    and creates one if it doesn't.
+
+    To automatically change the schema with changing requirements, ensure to
+    delete the schema file at the given path whenever the structure of DF
+    read from science db or the contents to be distributed are changed.
+
+    Parameters
+    ----------
+    df: DataFrame
+        A Spark DataFrame
+    schema_path: str
+        Path where to store the avro schema
+    """
+
+    # Check if the file exists
+    if not os.path.isfile(schema_path):
+        # Store the df as an avro file
+        path_for_avro = os.path.join(os.environ["PWD"], "flatten_hbase.avro")
+        if os.path.exists(path_for_avro):
+            shutil.rmtree(path_for_avro)
+        df.write.format("avro").save(path_for_avro)
+
+        # Read the avro schema from .avro file
+        avro_file = glob.glob(path_for_avro + "/part*")[0]
+        avro_schema = readschemafromavrofile(avro_file)
+
+        # Write the schema to a file for decoding Kafka messages
+        with open(schema_path, 'w') as f:
+            json.dump(avro_schema, f, indent=2)
+
+        # Remove .avro files and directory
+        shutil.rmtree(path_for_avro)
+    else:
+        msg = """
+            {} already exists - cannot write the new schema
+        """.format(schema_path)
+        print(msg)
+
+def readschemadata(bytes_io: io._io.BytesIO) -> fastavro._read.reader:
+    """Read data that already has an Avro schema.
+
+    Parameters
+    ----------
+    bytes_io : `_io.BytesIO`
+        Data to be decoded.
+
+    Returns
+    -------
+    `fastavro._read.reader`
+        Iterator over records (`dict`) in an avro file.
+
+    Examples
+    ----------
+    Open an avro file, and read the schema and the records
+    >>> with open(ztf_alert_sample, mode='rb') as file_data:
+    ...   data = readschemadata(file_data)
+    ...   # Read the schema
+    ...   schema = data.schema
+    ...   # data is an iterator
+    ...   for record in data:
+    ...     print(type(record))
+    <class 'dict'>
+    """
+    bytes_io.seek(0)
+    message = fastavro.reader(bytes_io)
+    return message
+
+def readschemafromavrofile(fn: str) -> dict:
+    """ Reach schema from a binary avro file.
+
+    Parameters
+    ----------
+    fn: str
+        Input Avro file with schema.
+
+    Returns
+    ----------
+    schema: dict
+        Dictionary (JSON) describing the schema.
+
+    Examples
+    ----------
+    >>> schema = readschemafromavrofile(ztf_alert_sample)
+    >>> print(schema['version'])
+    3.3
+    """
+    with open(fn, mode='rb') as file_data:
+        data = readschemadata(file_data)
+        schema = data.schema
+    return schema
+
+def write_to_kafka(sdf, key, kafka_bootstrap_servers, kafka_sasl_username, kafka_sasl_password, topic_name):
+    """ Send data to a Kafka cluster using Apache Spark
+
+    Parameters
+    ----------
+    sdf: Spark DataFrame
+        DataFrame
+    key: str
+        key for each Avro message
+    kafka_bootstrap_servers: str
+        Comma-separated list of ip:port of the Kafka machines
+    kafka_sasl_username: str
+        Username for writing into the Kafka cluster
+    kafka_sasl_password: str
+        Password for writing into the Kafka cluster
+    topic_name: str
+        Kafka topic (does not need to exist)
+    """
+    # Create a StructType column in the df for distribution.
+    df_struct = sdf.select(struct(sdf.columns).alias("struct"))
+    df_kafka = df_struct.select(to_avro("struct").alias("value"))
+    df_kafka = df_kafka.withColumn('key', key)
+
+    # Send schema
+    disquery = df_kafka\
+        .write\
+        .format("kafka")\
+        .option("kafka.bootstrap.servers", kafka_bootstrap_servers)\
+        .option("kafka.sasl.username", kafka_sasl_username)\
+        .option("kafka.sasl.password", kafka_sasl_password)\
+        .option("topic", topic_name)\
+        .save()
+
+def check_path_exist(dateToCheck):
+    """ Check we have data for the given night
+
+    Parameters
+    ----------
+    dateToCheck: str
+        YYYY-MM-DD
+
+    Returns
+    ----------
+    out: bool
+    """
+    r = requests.post(
+        'https://fink-portal.org/api/v1/statistics',
+        json={
+            'date': '{}{}{}'.format(*dateToCheck.split('-')),
+            'columns': 'basic:sci',
+            'output-format': 'json'
+        }
+    )
+    if r.json() == []:
+        return False
+    else:
+        return True
+
+def generate_spark_paths(dates, basePath):
+    """ Generate individual data paths
+
+    Parameters
+    ----------
+    dates: list of str
+        [startdate, stopdate] in the format YYYY-MM-DD
+    basePath: str
+        HDFS basepath for the data
+
+    Returns
+    ----------
+    paths: list of str
+        List of paths
+    """
+    endPath = '/science/year={}/month={}/day={}'
+    startDate = dates[0]
+    stopDate = dates[1]
+
+    if startDate == stopDate:
+        # easy case -- one night
+        if check_path_exist(startDate):
+            paths = [basePath + endPath.format(*startDate.split('-'))]
+        else:
+            paths = []
+    else:
+        # more than one night
+        dateRange = pd.date_range(
+            start=startDate,
+            end=stopDate
+        ).astype('str').values
+
+        paths = []
+        for aDate in dateRange:
+            if check_path_exist(aDate):
+                paths.append(basePath + endPath.format(*aDate.split('-')))
+
+    return paths
+
+def main(args):
+    spark = SparkSession.builder.getOrCreate()
+
+    paths = generate_spark_paths(args.dates, args.basePath)
+    if paths == []:
+        spark.stop()
+        sys.exit(1)
+
+    df = spark.read.format('parquet').option('basePath', args.basePath).load(paths)
+
+    # need fclass and extra conditions
+
+    if args.content == 'Full packet':
+        # Cast fields to ease the distribution
+        cnames = df.columns
+        cnames[cnames.index('timestamp')] = 'cast(timestamp as string) as timestamp'
+        cnames[cnames.index('cutoutScience')] = 'struct(cutoutScience.*) as cutoutScience'
+        cnames[cnames.index('cutoutTemplate')] = 'struct(cutoutTemplate.*) as cutoutTemplate'
+        cnames[cnames.index('cutoutDifference')] = 'struct(cutoutDifference.*) as cutoutDifference'
+        cnames[cnames.index('prv_candidates')] = 'explode(array(prv_candidates)) as prv_candidates'
+        cnames[cnames.index('candidate')] = 'struct(candidate.*) as candidate'
+    elif args.content == 'Lightcurve':
+        pass
+    elif args.content == 'Cutouts':
+        pass
+
+    # Wrap alert data
+    df = df.selectExpr(cnames)
+
+    # save schema
+    path_for_avro = 'new_schema_{}.avro'.format(time())
+    df.limit(1).write.format("avro").save(path_for_avro)
+
+    # retrieve data on local disk
+    subprocess.run(["hdfs", "dfs", '-get', path_for_avro])
+
+    # Read the avro schema from .avro file
+    avro_file = glob.glob(path_for_avro + "/part*")[0]
+    avro_schema = readschemafromavrofile(avro_file)
+
+    # Write the schema to a file for decoding Kafka messages
+    with open('/tmp/{}'.format(path_for_avro.replace('.avro', '.avsc')), 'w') as f:
+        json.dump(avro_schema, f, indent=2)
+
+
+    # reload the schema
+    with open('/tmp/{}'.format(path_for_avro.replace('.avro', '.avsc')), 'r') as f:
+        schema_ = json.dumps(f.read())
+
+    schema = json.loads(schema_)
+
+    # create a fake dataframe with 100 entries
+    df_schema = spark.createDataFrame(
+        pd.DataFrame(
+            {
+                'schema': [path_for_avro.replace('.avro', '.avsc')] * 1000
+            }
+        )
+    )
+
+    # Send schema
+    write_to_kafka(
+        df_schema,
+        lit(schema),
+        args.kafka_bootstrap_servers,
+        args.kafka_sasl_username,
+        args.kafka_sasl_password,
+        args.topic_name + '_schema'
+    )
+
+    # Send data
+    write_to_kafka(
+        df,
+        lit(args.topic_name),
+        args.kafka_bootstrap_servers,
+        args.kafka_sasl_username,
+        args.kafka_sasl_password,
+        args.topic_name
+    )
+
+
+if __name__ == "__main__":
+    """ Execute the test suite """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-dates', nargs=2)
+    parser.add_argument('-fclass', nargs='*')
+    parser.add_argument('-extraCond', nargs='*')
+    parser.add_argument('-content')
+    parser.add_argument('-basePath')
+    parser.add_argument('-topic_name')
+    parser.add_argument('-kafka_bootstrap_servers')
+    parser.add_argument('-kafka_sasl_username')
+    parser.add_argument('-kafka_sasl_password')
+
+    args = parser.parse_args()
+    main(args)
