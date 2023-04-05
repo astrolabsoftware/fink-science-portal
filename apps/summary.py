1b58eb0f8b7e09b1708c3229e9cd5986d568d06f
JulienPeloton <peloton@lal.in2p3.fr>
2023-04-05 14:10:46 +0200

    Add buttons for other brokers

--- apps/summary.py
+++ apps/summary.py
@@ -613,7 +613,7 @@ def accordion_mobile():
                         icon=[
                             DashIconify(
                                 icon="tabler:external-link",
-                                color=dmc.theme.DEFAULT_COLORS["red"][6],
+                                color=dmc.theme.DEFAULT_COLORS["gray"][6],
                                 width=20,
                             )
                         ],
@@ -839,35 +839,35 @@ def integrate_aladin_lite_mobile(object_data, states):
     alert_id: str
         ID of the alert
     """
-    if states['2']:
-        pdf_ = pd.read_json(object_data)
-        cols = ['i:jd', 'i:ra', 'i:dec']
-        pdf = pdf_.loc[:, cols]
-        pdf = pdf.sort_values('i:jd', ascending=False)
+    if states is not None:
+        if states['2']:
+            pdf_ = pd.read_json(object_data)
+            cols = ['i:jd', 'i:ra', 'i:dec']
+            pdf = pdf_.loc[:, cols]
+            pdf = pdf.sort_values('i:jd', ascending=False)
 
-        # Coordinate of the current alert
-        ra0 = pdf['i:ra'].values[0]
-        dec0 = pdf['i:dec'].values[0]
+            # Coordinate of the current alert
+            ra0 = pdf['i:ra'].values[0]
+            dec0 = pdf['i:dec'].values[0]
 
-        # Javascript. Note the use {{}} for dictionary
-        img = """
-        var aladin = A.aladin('#aladin-lite-div2',
-                  {{
-                    survey: 'P/PanSTARRS/DR1/color/z/zg/g',
-                    fov: 0.025,
-                    target: '{} {}',
-                    reticleColor: '#ff89ff',
-                    reticleSize: 32
-        }});
-        var cat = 'https://axel.u-strasbg.fr/HiPSCatService/Simbad';
-        var hips = A.catalogHiPS(cat, {{onClick: 'showTable', name: 'Simbad'}});
-        aladin.addCatalog(hips);
-        """.format(ra0, dec0)
+            # Javascript. Note the use {{}} for dictionary
+            img = """
+            var aladin = A.aladin('#aladin-lite-div2',
+                    {{
+                        survey: 'P/PanSTARRS/DR1/color/z/zg/g',
+                        fov: 0.025,
+                        target: '{} {}',
+                        reticleColor: '#ff89ff',
+                        reticleSize: 32
+            }});
+            var cat = 'https://axel.u-strasbg.fr/HiPSCatService/Simbad';
+            var hips = A.catalogHiPS(cat, {{onClick: 'showTable', name: 'Simbad'}});
+            aladin.addCatalog(hips);
+            """.format(ra0, dec0)
 
-        # img cannot be executed directly because of formatting
-        # We split line-by-line and remove comments
-        img_to_show = [i for i in img.split('\n') if '// ' not in i]
+            # img cannot be executed directly because of formatting
+            # We split line-by-line and remove comments
+            img_to_show = [i for i in img.split('\n') if '// ' not in i]
 
-        return " ".join(img_to_show)
-    else:
-        return ''
+            return " ".join(img_to_show)
+    return ''
