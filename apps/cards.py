de2c8fc206a5a2ad85019638813b5225cc08638e
JulienPeloton <peloton@lal.in2p3.fr>
2023-06-05 07:35:00 +0200

    Layout

--- apps/cards.py
+++ apps/cards.py
@@ -228,7 +228,7 @@ def create_external_links(ra0, dec0):
                 dbc.Col(
                     dbc.Button(
                         className='btn btn-default btn-circle btn-lg',
-                        style={'background-image': 'url(/assets/buttons/vsx.png)', 'background-size': 'contain'},
+                        style={'background-image': 'url(/assets/buttons/vsx.png)', 'background-size': 'contain', 'background-repeat': 'no-repeat'},
                         color='white',
                         outline=True,
                         id='VSX',
