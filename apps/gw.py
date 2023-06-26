031b750e7d1db9c0c5accbef6adb1c4be872eaa6
JulienPeloton <peloton@lal.in2p3.fr>
2023-06-26 10:23:39 +0200

    Import

--- apps/gw.py
+++ apps/gw.py
@@ -26,7 +26,7 @@ import pandas as pd
 from urllib.request import urlopen, URLError
 
 from app import app, APIURL
-from apps.utils import markdownify_objectid, convert_jd, simbad_types
+from apps.utils import markdownify_objectid, convert_jd, simbad_types, class_colors
 
 @app.callback(
     Output("gw-data", "data"),
