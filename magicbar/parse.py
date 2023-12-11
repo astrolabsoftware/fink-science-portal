99e355e348468e8526f8bb207a74a874d1f7b764
Sergey Karpov <karpov.sv@gmail.com>
2023-12-11 15:22:36 +0100

    Local changes for smartsearch

---
+++ magicbar/parse.py
@@ -0,0 +1,275 @@
+import shlex
+import regex as re # For partial matching
+import requests
+import functools
+
+APIURL = 'https://fink-portal.org'
+
+@functools.lru_cache(maxsize=320)
+def call_resolver(data, kind, timeout=None):
+    """ Call Fink resolver
+
+    Parameters
+    ----------
+    data: str
+        Query payload
+    kind: str
+        Resolver name: ssodnet, tns, simbad
+
+    Returns
+    ----------
+    payload: dict
+        Payload returned by the /api/v1/resolver endpoint
+    """
+
+    if kind == 'tns':
+        # Normalize AT name to have whitespace before year
+        m = re.match('^(AT)\s*(2\w+)$', data)
+        if m:
+            data = m[1] + ' ' + m[2]
+
+    try:
+        if kind == 'ztf':
+            r = requests.post(
+                '{}/api/v1/objects'.format(APIURL),
+                json={
+                    'objectId': data,
+                    'columns': "i:ra,i:dec",
+                },
+                timeout=timeout
+            )
+        else:
+            r = requests.post(
+                '{}/api/v1/resolver'.format(APIURL),
+                json={
+                    'resolver': kind,
+                    'name': str(data),
+                },
+                timeout=timeout
+            )
+
+        payload = r.json()
+    except requests.exceptions.ReadTimeout:
+        payload = None
+
+    return payload
+
+name_patterns = [
+    {
+        'type': 'ztf',
+        'pattern': '^ZTF[12]\d\w{7}$',
+        'hint': 'ZTF objectId: ZTFyyccccccc',
+        'min': 3
+    },
+    {
+        'type': 'tracklet',
+        'pattern': '^TRCK_\d{8}_\d{6}_\d{2}$',
+        'hint': 'tracklet: TRCK_YYYYMMDD_HHMMSS_NN',
+        'min': 4
+    },
+    # {
+    #     'type': 'at',
+    #     'pattern': '^AT[12]\d{3}\w{3}$',
+    #     'hint': 'AT: ATyyyyccc',
+    #     'min': 3
+    # },
+]
+
+def parse_query(string, timeout=None):
+    """ Parse (probably incomplete) query
+
+    Order is as follows:
+    1. Extract object names (partially) matching some patterns
+    2. Extract keyword parameters, either key:value or key=value
+    3. Try to interpret the rest as coordinates as follows:
+      - Pair of degrees
+      - HH MM SS.S [+-]?DD MM SS.S
+      - HH:MM:SS.S [+-]?DD:MM:SS.S
+      - HHhMMhSS.Ss [+-]?DDhMMhSS.Ss
+      - optionally, use one more number as a radius, in either arcseconds, minutes or degrees
+    4. The rest is resolved through several Fink resolvers
+    5. Finally, the action is suggested based on the parameters
+      - for ZTF objectIds it is 'objectid' unless the radius `r` is explicitly given and the match is not partial (then it is 'conesearch')
+      - for tracklets, it is always 'tracklet'
+      - for resolved objects or coordinates, it is `conesearch` even if no radius is specified
+      - for SSO objects it is always `sso`
+
+    Parameters
+    ----------
+    string: str
+        String to parse
+
+    Returns
+    ----------
+    Dictionary containing the following keys:
+      - object: object name
+      - type: object type derived from name parsing
+      - hint: some human-readable description of what was parsed
+      - action: suggested action for the query
+      - params: dictionary with keyword parameters (ra, dec, r, ...)
+      - string: original query string
+    """
+    # Results schema
+    query = {
+        'object': None,
+        'type': None,
+        'partial': False,
+        'hint': None,
+        'action': None,
+        'params': {},
+        'string': string,
+    }
+
+    string = string.replace(',', ' ') # TODO: preserve quoted commas?..
+
+    try:
+        tokens = shlex.split(string, posix=True) # It will also handle quoted strings
+    except:
+        return query
+
+    unparsed = []
+
+    for token in tokens:
+        is_parsed = False
+        # Try to locate well-defined object name patterns
+        for pattern in name_patterns:
+            if pattern.get('min') and len(token) >= pattern.get('min'):
+                m = re.match(pattern['pattern'], token, partial=True)
+                if m:
+                    query['object'] = token
+                    query['type'] = pattern['type']
+                    query['hint'] = pattern['hint']
+                    query['partial'] = m.partial
+                    is_parsed = True
+                    break
+
+        if is_parsed:
+            continue
+
+        # Try to parse keyword parameters, either as key:value or key=value
+        m = re.match('^(\w+)[=:]([^:=]*?)$', token)
+        if m:
+            key = m[1]
+            value = m[2]
+            # Special handling for numbers, possibly ending with d/m/s for degrees etc
+            m = re.match('^([+-]?(\d+)(.\d+)?)([dms\'"]?)$', value)
+            if m:
+                value = float(m[1])
+                if m[4] == 'd':
+                    value /= 1
+                elif m[4] == 'm' or m[4] == '\'':
+                    value /= 60
+                elif m[4] == 's' or m[4] == '"':
+                    value /= 3600
+                else:
+                    # Default is no change, except for 'r' key
+                    if key == 'r':
+                        value /= 3600
+
+            query['params'][key] = value
+
+        else:
+            unparsed.append(token)
+
+    string = " ".join(unparsed)
+
+    # Parse the rest of the query string as coordinates, if any
+    if len(string) and not query['object']:
+        # Pair of decimal degrees
+        m = re.search("^(\d+\.?\d*)\s+([+-]?\d+\.?\d*)(\s+(\d+\.?\d*))?$", string)
+        if m:
+            query['params']['ra'] = float(m[1])
+            query['params']['dec'] = float(m[2])
+            if m[4] is not None:
+                query['params']['r'] = float(m[4])/3600
+
+            query['object'] = string
+            query['type'] = 'coordinates'
+            query['hint'] = 'Decimal coordinates'
+
+            if m[4] is not None:
+                query['hint'] += ' with radius'
+
+        else:
+            # HMS DMS
+            m = re.search(
+                "^(\d{1,2})\s+(\d{1,2})\s+(\d{1,2}\.?\d*)\s+([+-])?\s*(\d{1,3})\s+(\d{1,2})\s+(\d{1,2}\.?\d*)(\s+(\d+\.?\d*))?$",
+                string
+            ) or re.search(
+                "^(\d{1,2})[:h](\d{1,2})[:m](\d{1,2}\.?\d*)[s]?\s+([+-])?\s*(\d{1,3})[d:](\d{1,2})[m:](\d{1,2}\.?\d*)[s]?(\s+(\d+\.?\d*))?$",
+                string
+            )
+            if m:
+                query['params']['ra'] = (float(m[1]) + float(m[2])/60 + float(m[3])/3600)*15
+                query['params']['dec'] = (float(m[5]) + float(m[6])/60 + float(m[7])/3600)
+
+                if m[4] == '-':
+                    query['params']['dec'] *= -1
+
+                if m[9] is not None:
+                    query['params']['r'] = float(m[9])/3600
+
+                query['object'] = string
+                query['type'] = 'coordinates'
+                query['hint'] = 'HMS DMS coordinates'
+
+                if m[9] is not None:
+                    query['hint'] += ' with radius'
+
+            else:
+                query['object'] = string
+                query['type'] = 'unresolved'
+
+    # Should we resolve object name?..
+    if query['object'] and query['type'] == 'ztf' and not query['partial'] and 'r' in query['params']:
+        res = call_resolver(query['object'], 'ztf')
+        if res:
+            query['params']['ra'] = res[0]['i:ra']
+            query['params']['dec'] = res[0]['i:dec']
+
+    if query['object'] and query['type'] not in ['ztf', 'tracklet', 'coordinates', None]:
+        # Simbad
+        if query['object'][0].isalpha():
+            res = call_resolver(query['object'], 'simbad', timeout=timeout)
+            if res:
+                query['object'] = res[0]['oname']
+                query['type'] = 'simbad'
+                query['hint'] = 'Simbad object'
+                query['params']['ra'] = res[0]['jradeg']
+                query['params']['dec'] = res[0]['jdedeg']
+
+        if 'ra' not in query['params'] and query['object'][0].isalpha():
+            # TNS
+            res = call_resolver(query['object'], 'tns', timeout=timeout)
+            if res:
+                query['object'] = res[0]['d:fullname']
+                query['type'] = 'tns'
+                query['hint'] = 'TNS object'
+                query['params']['ra'] = res[0]['d:ra']
+                query['params']['dec'] = res[0]['d:declination']
+
+        if 'ra' not in query['params']:
+                # SSO - final test
+                res = call_resolver(query['object'], 'ssodnet', timeout=timeout)
+                if res:
+                    query['object'] = res[0]['name']
+                    query['type'] = 'ssodnet'
+                    query['hint'] = 'SSO object / {}'.format(res[0]['type'])
+
+    # Guess the kind of query
+    if 'ra' in query['params'] and 'dec' in query['params']:
+        query['action'] = 'conesearch'
+
+    elif query['type'] == 'ztf':
+        query['action'] = 'objectid'
+
+    elif query['type'] == 'tracklet':
+        query['action'] = 'tracklet'
+
+    elif query['type'] == 'ssodnet':
+        query['action'] = 'sso'
+
+    else:
+        query['action'] = 'unknown'
+
+    return query
---
+++ magicbar/smartsearch.py
@@ -0,0 +1,229 @@
+import dash
+from dash import html, dcc, Input, Output, State, dash_table, no_update, ctx, clientside_callback
+import requests
+import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+import json
+
+from dash_autocomplete_input import AutocompleteInput
+
+from fink_utils.xmatch.simbad import get_simbad_labels
+import pandas as pd
+
+simbad_types = get_simbad_labels('old_and_new')
+simbad_types = sorted(simbad_types, key=lambda s: s.lower())
+
+tns_types = pd.read_csv('../assets/tns_types.csv', header=None)[0].values
+tns_types = sorted(tns_types, key=lambda s: s.lower())
+
+finkclasses = [
+    'Unknown',
+    'Early Supernova Ia candidates',
+    'Supernova candidates',
+    'Kilonova candidates',
+    'Microlensing candidates',
+    'Solar System (MPC)',
+    'Solar System (candidates)',
+    'Tracklet (space debris & satellite glints)',
+    'Ambiguous',
+    *['(TNS) ' + t for t in tns_types],
+    *['(SIMBAD) ' + t for t in simbad_types]
+]
+
+# bootstrap theme
+external_stylesheets = [
+    dbc.themes.SPACELAB,
+    '//aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.css',
+    '//use.fontawesome.com/releases/v5.7.2/css/all.css',
+]
+external_scripts = [
+    '//code.jquery.com/jquery-1.12.1.min.js',
+    '//aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js',
+    '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML',
+]
+
+from telemetry import DashWithTelemetry
+app = DashWithTelemetry(
+# app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets,
+    external_scripts=external_scripts,
+    meta_tags=[{
+        "name": "viewport",
+        "content": "width=device-width, initial-scale=1"
+    }]
+)
+
+####################################
+# Smart search bar
+
+fink_search_bar = dbc.Row(dbc.Col(
+    className='p-0 m-0 border border-dark rounded-3',
+    children=[
+        dbc.InputGroup(
+            [
+                # dcc.Input(
+                #     id='magic_search',
+                #     value='',
+                #     autoFocus=True,
+                #     type='text',
+                #     className='inputbar form-control border-0',
+                # ),
+                AutocompleteInput(
+                    id='magic_search',
+                    component='input',
+                    trigger=['class:'],
+                    # options={'class:':['class1', 'new class', 'next class', "type of all things"]},
+                    options={'class:':finkclasses},
+                    maxOptions=0,
+                    className="inputbar form-control border-0",
+                    quoteWhitespaces=True,
+                    regex='^([a-zA-Z0-9_\-()]+|"[a-zA-Z0-9_\- ]*|\'[a-zA-Z0-9_\- ]*)$',
+                    autoFocus=True,
+                ),
+
+                dbc.Spinner([
+                    dmc.ActionIcon(
+                        DashIconify(icon="tabler:search", width=20),
+                        n_clicks=0,
+                        id="submit",
+                        color='gray',
+                        variant="transparent",
+                        radius='xl',
+                        size='lg',
+                        loaderProps={'variant': 'dots', 'color': 'orange'},
+                        # Hide on screen sizes smaller than sm
+                        className="d-none d-sm-flex"
+                    ),
+                ], size='sm',),
+            ],
+        ),
+        dbc.ListGroup(
+            id='magic_suggest',
+        ),
+        dcc.Interval(id="magic_timer", interval=2000, max_intervals=1, disabled=True)
+    ]
+))
+
+from parse import parse_query
+
+# Time-based debounce from https://joetatusko.com/2023/07/11/time-based-debouncing-with-plotly-dash/
+# @app.callback(
+#     Output('magic_timer', 'n_intervals'),
+#     Output('magic_timer', 'disabled'),
+#     Input('magic_search', 'value'),
+#     Input('magic_search', 'n_submit'),
+#     prevent_initial_call=True,
+# )
+# def start_suggestion_debounce_timer(value, n_submit):
+#     if ctx.triggered[0]['prop_id'].split('.')[1] == 'n_submit':
+#         # Stop timer on submit
+#         return no_update, True
+#     else:
+#         # Start timer on normal input
+#         return 0, False
+
+clientside_callback(
+    """
+    function start_suggestion_debounce_timer(value, n_submit) {
+        const triggered = dash_clientside.callback_context.triggered.map(t => t.prop_id);
+        if (triggered == 'magic_search.n_submit')
+            return [dash_clientside.no_update, true];
+        return [0, false];
+    }
+    """,
+    [
+        Output('magic_timer', 'n_intervals'),
+        Output('magic_timer', 'disabled')
+    ],
+    Input('magic_search', 'value'),
+    Input('magic_search', 'n_submit'),
+    prevent_initial_call=True,
+)
+
+
+# Update suggestions
+@app.callback(
+    Output('magic_suggest', 'children'),
+    Output('submit', 'children'),
+    Input('magic_timer', 'n_intervals'),
+    State('magic_search', 'value'),
+    prevent_initial_call=True,
+)
+def update_suggestions(n_intervals, value):
+    if n_intervals == 1:
+        if not value:
+            return None, no_update
+        query = parse_query(value, timeout=5)
+        suggestions = []
+
+        params = query['params']
+
+        if not query['action']:
+            return None, no_update
+
+        content = [
+            dmc.Group([
+                html.Strong(query['object']) if query['object'] else None,
+                dmc.Badge(query['type'], variant="outline", color='blue') if query['type'] else None,
+                dmc.Badge(query['action'], variant="outline", color='red'),
+            ], noWrap=False, position='left'),
+            html.P(query['hint'], className='m-0'),
+        ]
+
+        if len(params):
+            content.append(
+                html.Small(" ".join(["{}={}".format(_,params[_]) for _ in params]))
+            )
+
+        suggestion = dbc.ListGroupItem(
+            content,
+            action=True,
+            className='border-0'
+        )
+
+        suggestions.append(suggestion)
+
+        return suggestions, no_update
+    else:
+        return no_update, no_update
+
+# Submit the results
+@app.callback(
+    Output('check', 'children'),
+    Input('magic_search', 'n_submit'),
+    State('magic_search', 'value'),
+)
+def on_submit(n_submit, value):
+    if not n_submit:
+        raise PreventUpdate
+    else:
+        print('query:', value)
+
+        if value:
+            query = parse_query(value)
+            return html.Span(str(query), className='text-danger')
+        else:
+            return no_update
+
+#############################################
+
+# Main app
+app.layout = html.Div(
+    [
+        dbc.Container(
+            [
+                html.Br(),
+                dbc.Row(dbc.Col(fink_search_bar, width=12)),
+                html.Div(id='check'),
+                dmc.Space(h=20),
+                html.Pre(parse_query.__doc__),
+            ]
+        )
+    ]
+)
+
+# app.run_server('159.69.107.239', debug=True)
+app.run_server(debug=True)
