45ab4e529156e7af030599f2d6fe0f73eb91c55a
JulienPeloton <peloton@lal.in2p3.fr>
2024-01-11 07:51:38 +0100

    Add options

--- apps/plotting.py
+++ apps/plotting.py
@@ -2490,7 +2490,9 @@ def integrate_aladin_lite(object_data):
                 fov: 0.025,
                 target: '{} {}',
                 reticleColor: '#ff89ff',
-                reticleSize: 32
+                reticleSize: 32,
+                showContextMenu: true,
+                showCooGrid: true,
     }});
     var cat = 'https://axel.u-strasbg.fr/HiPSCatService/Simbad';
     var hips = A.catalogHiPS(cat, {{onClick: 'showTable', name: 'Simbad'}});
