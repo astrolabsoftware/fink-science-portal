d0a9ee247637fcaa6f4d765e010338f80d24e1e3
Julien <peloton@lal.in2p3.fr>
2024-01-29 21:09:50 +0100

    Aladin Lite v3 (#553)
    * Migrate external Aladin to v3, and remove external stylesheet (embedded in the JS file)
    
    * Add options
    
    * Add Grid Control
    
    * Rename PanSTARRS layer
    
    * Use URL provided in tutorial
    
    * Rename survey
    
    * Use the URL for the survey parameter of the Aladin lite

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 AstroLab Software
+# Copyright 2021-2024 AstroLab Software
 # Author: Julien Peloton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,12 +35,11 @@ LOCALAPI = args.get('LOCALAPI', False)
 # bootstrap theme
 external_stylesheets = [
     dbc.themes.SPACELAB,
-    '//aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.css',
     '//use.fontawesome.com/releases/v5.7.2/css/all.css',
 ]
 external_scripts = [
     '//code.jquery.com/jquery-1.12.1.min.js',
-    '//aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js',
+    '//aladin.u-strasbg.fr/AladinLite/api/v3/latest/aladin.js',
     '//cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
 ]
 
