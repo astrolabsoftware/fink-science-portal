e19a00eae0021c6996b14eb428574eba6aa96526
JulienPeloton <peloton@lal.in2p3.fr>
2024-02-06 13:23:40 +0100

    Update schema for /columns

--- apps/api/api.py
+++ apps/api/api.py
@@ -943,15 +943,35 @@ def columns_arguments():
             {'name': 'cdsxmatch', 'type': 'string', 'doc': 'Object type of the closest source from SIMBAD database; if exists within 1 arcsec. See https://fink-portal.org/api/v1/classes'},
             {'name': 'gcvs', 'type': 'string', 'doc': 'Object type of the closest source from GCVS catalog; if exists within 1 arcsec.'},
             {'name': 'vsx', 'type': 'string', 'doc': 'Object type of the closest source from VSX catalog; if exists within 1 arcsec.'},
+            {'name': 'DR3Name', 'type': 'string', 'doc': 'Unique source designation of closest source from Gaia catalog; if exists within 1 arcsec.'},
             {'name': 'Plx', 'type': 'double', 'doc': 'Absolute stellar parallax (in milli-arcsecond) of the closest source from Gaia catalog; if exists within 1 arcsec.'},
             {'name': 'e_Plx', 'type': 'double', 'doc': 'Standard error of the stellar parallax (in milli-arcsecond) of the closest source from Gaia catalog; if exists within 1 arcsec.'},
-            {'name': 'DR3Name', 'type': 'string', 'doc': 'Unique source designation of closest source from Gaia catalog; if exists within 1 arcsec.'},
+            {'name': 'x3hsp', 'type': 'string', 'doc': 'Counterpart (cross-match) to the 3HSP catalog if exists within 1 arcminute.'},
+            {'name': 'x4lac', 'type': 'string', 'doc': 'Counterpart (cross-match) to the 4LAC DR3 catalog if exists within 1 arcminute.'},
+            {'name': 'mangrove_HyperLEDA_name', 'type': 'string', 'doc': 'HyperLEDA source designation of closest source from Mangrove catalog; if exists within 1 arcmin.'},
+            {'name': 'mangrove_2MASS_name', 'type': 'string', 'doc': '2MASS source designation of closest source from Mangrove catalog; if exists within 1 arcmin.'},
+            {'name': 'mangrove_lum_dist', 'type': 'string', 'doc': 'Luminosity distance of closest source from Mangrove catalog; if exists within 1 arcmin.'},
+            {'name': 'mangrove_ang_dist', 'type': 'string', 'doc': 'Angular distance of closest source from Mangrove catalog; if exists within 1 arcmin.'},
             {'name': 'mulens', 'type': 'double', 'doc': 'Probability score of an alert to be a microlensing event by [LIA](https://github.com/dgodinez77/LIA).'},
             {'name': 'rf_snia_vs_nonia', 'type': 'double', 'doc': 'Probability of an alert to be a SNe Ia using a Random Forest Classifier (binary classification). Higher is better.'},
             {'name': 'rf_kn_vs_nonkn', 'type': 'double', 'doc': 'Probability of an alert to be a Kilonova using a PCA & Random Forest Classifier (binary classification). Higher is better.'},
             {'name': 'roid', 'type': 'int', 'doc': 'Determine if the alert is a potential Solar System object (experimental). 0: likely not SSO, 1: first appearance but likely not SSO, 2: candidate SSO, 3: found in MPC.'},
             {'name': 'snn_sn_vs_all', 'type': 'double', 'doc': 'The probability of an alert to be a SNe vs. anything else (variable stars and other categories in the training) using SuperNNova'},
             {'name': 'snn_snia_vs_nonia', 'type': 'double', 'doc': 'The probability of an alert to be a SN Ia vs. core-collapse SNe using SuperNNova'},
+            {'name': 'anomaly_score', 'type': 'double', 'doc': 'Probability of an alert to be anomalous (lower values mean more anomalous observations) based on lc_*'},
+            {'name': 'nalerthist', 'type': 'int', 'doc': 'Number of detections contained in each alert (current+history). Upper limits are not taken into account.'},
+            {'name': 'tracklet', 'type': 'string', 'doc': 'ID for fast moving objects, typically orbiting around the Earth. Of the format YYYY-MM-DD hh:mm:ss'},
+            {'name': 'lc_features_g', 'type': 'string', 'doc': 'Numerous light curve features for the g band (see https://github.com/astrolabsoftware/fink-science/tree/master/fink_science/ad_features). Stored as string of array.'},
+            {'name': 'lc_features_r', 'type': 'string', 'doc': 'Numerous light curve features for the r band (see https://github.com/astrolabsoftware/fink-science/tree/master/fink_science/ad_features). Stored as string of array.'},
+            {'name': 'jd_first_real_det', 'type': 'double', 'doc': 'First variation time at 5 sigma contained in the alert history'},
+            {'name': 'jdstarthist_dt', 'type': 'double', 'doc': 'Delta time between `jd_first_real_det` and the first variation time at 3 sigma (`jdstarthist`). If `jdstarthist_dt` > 30 days then the first variation time at 5 sigma is False (accurate for fast transient).'},
+            {'name': 'mag_rate', 'type': 'double', 'doc': 'Magnitude rate (mag/day)'},
+            {'name': 'sigma_rate', 'type': 'double', 'doc': 'Magnitude rate error estimation (mag/day)'},
+            {'name': 'lower_rate', 'type': 'double', 'doc': '5% percentile of the magnitude rate sampling used for the error computation (`sigma_rate`)'},
+            {'name': 'upper_rate', 'type': 'double', 'doc': '95% percentile of the magnitude rate sampling used for the error computation (`sigma_rate`)'},
+            {'name': 'delta_time', 'type': 'double', 'doc': 'Delta time between the the two measurement used for the magnitude rate `mag_rate`'},
+            {'name': 'from_upper', 'type': 'boolean', 'doc': 'If True, the magnitude rate `mag_rate` has been computed using the last upper limit and the current measurement.'},
+            {'name': 'tag', 'type': 'string', 'doc': 'Quality tag among `valid`, `badquality` (does not satisfy quality cuts), and `upper` (upper limit measurement). Only available if `withupperlim` is set to True.'}
         ]
     )
 
@@ -967,7 +987,7 @@ def columns_arguments():
             {'name': 'sigma(rate)', 'type': 'double', 'doc': 'Error of brightness change rate in mag/day (between last and previous measurement in this filter).'},
             {'name': 'lastdate', 'type': 'string', 'doc': 'Human readable datetime for the alert (from the i:jd field).'},
             {'name': 'firstdate', 'type': 'string', 'doc': 'Human readable datetime for the first detection of the object (from the i:jdstarthist field).'},
-            {'name': 'lapse', 'type': 'string', 'doc': 'Number of days between first and last detection.'},
+            {'name': 'lapse', 'type': 'double', 'doc': 'Number of days between first and last detection.'},
         ]
     )
 
@@ -980,7 +1000,7 @@ def columns_arguments():
         'ZTF original fields (i:)': {i: {'type': j, 'doc': k} for i, j, k in zip(ztf_candidate.name, ztf_candidate.type, ztf_candidate.doc)},
         'ZTF original cutouts (b:)': {i: {'type': j, 'doc': k} for i, j, k in zip(ztf_cutouts.name, ztf_cutouts.type, ztf_cutouts.doc)},
         'Fink science module outputs (d:)': {i: {'type': j, 'doc': k} for i, j, k in zip(fink_science.name, fink_science.type, fink_science.doc)},
-        'Fink added values (v:)': {i: {'type': j, 'doc': k} for i, j, k in zip(fink_derived.name, fink_derived.type, fink_derived.doc)}
+        'Fink on-the-fly added values (v:)': {i: {'type': j, 'doc': k} for i, j, k in zip(fink_derived.name, fink_derived.type, fink_derived.doc)}
     }
 
     return jsonify({'fields': types})
