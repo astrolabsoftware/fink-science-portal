ff98d88f05e350ee4d3e473aa8ca98046fbeb711
Julien <peloton@lal.in2p3.fr>
2024-03-27 12:46:38 +0100

    Freeze the Aladin version to 3.2.0 (#607)

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ external_stylesheets = [
 ]
 external_scripts = [
     '//code.jquery.com/jquery-1.12.1.min.js',
-    '//aladin.u-strasbg.fr/AladinLite/api/v3/latest/aladin.js',
+    '//aladin.u-strasbg.fr/AladinLite/api/v3/3.2.0/aladin.js',
     '//cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
 ]
 
