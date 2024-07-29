7e6a86be20535fdcd3ec25407d9b9c931b16a4f1
JulienPeloton <peloton@lal.in2p3.fr>
2024-07-29 13:32:49 +0200

    Align tabs

--- apps/summary.py
+++ apps/summary.py
@@ -515,7 +515,7 @@ def tabs(pdf):
                     ),
                     dmc.TabsTab("GRB", value="GRB", disabled=True),
                 ],
-                right=True,
+                position="right",
             ),
             dmc.TabsPanel(children=[tab1_content(pdf)], value="Summary"),
             dmc.TabsPanel(tab2_content(), value="Supernovae"),
