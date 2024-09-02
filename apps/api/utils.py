b0f1094e35722b99eae43bc9d1c818589893876a
JulienPeloton <peloton@lal.in2p3.fr>
2024-09-02 16:32:28 +0200

    Fix typo

--- apps/api/utils.py
+++ apps/api/utils.py
@@ -371,7 +371,7 @@ def return_explorer_pdf(payload: dict, user_group: int) -> pd.DataFrame:
 
     return pdfs
 
-def return_conesearch_pdf(payload: dict, user_group: int) -> pd.DataFrame:
+def return_conesearch_pdf(payload: dict) -> pd.DataFrame:
     """Extract data returned by HBase and format it in a Pandas dataframe
 
     Data is from /api/v1/conesearch
