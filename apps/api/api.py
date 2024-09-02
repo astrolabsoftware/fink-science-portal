13ea6ce4ee0bae5d5533522002c99b3668775e48
JulienPeloton <peloton@lal.in2p3.fr>
2024-09-02 15:10:53 +0200

    Fix indent

--- apps/api/api.py
+++ apps/api/api.py
@@ -83,41 +83,43 @@ def layout():
                 style={"height": "100%", "width": "100%"},
                 children=[
                     dmc.Card(
-                        dmc.CardSection(
-                            dmc.Image(
-                                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
-                                h=160,
-                                alt="Norway",
-                            )
-                        ),
-                        dmc.Group(
-                            [
-                                dmc.Text("Norway Fjord Adventures", fw=500),
-                                dmc.Badge("On Sale", color="pink"),
-                            ],
-                            justify="space-between",
-                            mt="md",
-                            mb="xs",
-                        ),
-                        dmc.Text(
-                            "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and "
-                            "around the fjords of Norway",
-                            size="sm",
-                            c="dimmed",
-                        ),
-                        dmc.Button(
-                            "Book classic tour now",
-                            color="blue",
-                            fullWidth=True,
-                            mt="md",
-                            radius="md",
-                        ),
-                    ),
+                        children=[
+                            dmc.CardSection(
+                                dmc.Image(
+                                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+                                    h=160,
+                                    alt="Norway",
+                                )
+                            ),
+                            dmc.Group(
+                                [
+                                    dmc.Text("Norway Fjord Adventures", fw=500),
+                                    dmc.Badge("On Sale", color="pink"),
+                                ],
+                                justify="space-between",
+                                mt="md",
+                                mb="xs",
+                            ),
+                            dmc.Text(
+                                "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and "
+                                "around the fjords of Norway",
+                                size="sm",
+                                c="dimmed",
+                            ),
+                            dmc.Button(
+                                "Book classic tour now",
+                                color="blue",
+                                fullWidth=True,
+                                mt="md",
+                                radius="md",
+                            ),
+                        ],
+                        withBorder=True,
+                        shadow="sm",
+                        radius="md",
+                        w=350,
+                    )
                 ],
-                withBorder=True,
-                shadow="sm",
-                radius="md",
-                w=350,
             ),
             fluid=True,
             className="home",
