7482bd6c3167d30b4c316e9541e7daf2da7ca301
JulienPeloton <peloton@lal.in2p3.fr>
2024-10-01 16:51:13 +0200

    PEP8

--- apps/summary.py
+++ apps/summary.py
@@ -17,7 +17,6 @@ import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 import numpy as np
 import pandas as pd
-import rocks
 import visdcc
 from dash import Input, Output, State, dcc, html, no_update
 from dash.exceptions import PreventUpdate
@@ -615,7 +614,7 @@ def store_query(name):
             # Extract miriade information as well
             # TODO: understand the impact of the two lines below
             # name = rocks.id(payload)[0]
-            #if name:
+            # if name:
             #    pdfsso["i:ssnamenr"] = name
 
             pdfsso = get_miriade_data(pdfsso, withecl=False)
