1caf9e335f911d155d2b57b8ab0eca2908be661a
JulienPeloton <peloton@lal.in2p3.fr>
2024-10-22 12:13:57 +0200

    Remove wrong argument

--- apps/sso/cards.py
+++ apps/sso/cards.py
@@ -562,7 +562,7 @@ def card_sso_rocks_params(data):
                     ),
                 ),
                 dmc.HoverCardDropdown(
-                    dcc.Markdown(f"Best estimates of the dynamical and physical properties of the object from the [ssoCard](https://ssp.imcce.fr/forms/ssocard/{data.id_}) compiled by the [SsODNet service](https://ssp.imcce.fr/webservices/ssodnet/).", size="md")
+                    dcc.Markdown(f"Best estimates of the dynamical and physical properties of the object from the [ssoCard](https://ssp.imcce.fr/forms/ssocard/{data.id_}) compiled by the [SsODNet service](https://ssp.imcce.fr/webservices/ssodnet/).")
                 ),
             ],
         ),
