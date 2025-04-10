3f216418f1c68efc4386bdedf854940d686f3d60
Julien <peloton@lal.in2p3.fr>
2025-04-10 15:14:32 +0200

    Bump to v6 (#724)

--- assets/spark_ztf_transfer.py
+++ assets/spark_ztf_transfer.py
@@ -21,7 +21,7 @@ from pyspark.sql.functions import struct, lit
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql.types import StringType
 
-from fink_filters.classification import extract_fink_classification
+from fink_filters.ztf.classification import extract_fink_classification
 from fink_utils.spark import schema_converter
 from fink_utils.spark.utils import concat_col
 from pyspark.sql import functions as F
